69abac5b0c00fb4b097292ed43442a1fa0f9f01d
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2022-03-29 21:14:43 +0800

    Update 刘志进实验室.js

--- sources/刘志进实验室.js
+++ sources/刘志进实验室.js
@@ -35,7 +35,7 @@ function mainifest() {
 		tag: "音乐",
 		
 		//@NonNull 详细界面域名，搜索源标识
-		host: "liuzhijin.com"
+		host: "music.liuzhijin.com"
 	});
 }
 const header = '@header->x-requested-with:XMLHttpRequest@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
