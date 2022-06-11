b2385e66f0ff96301abff64f2e85b1bf16f72132
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2022-06-11 11:18:06 +0800

    Add files via upload

--- sources/多多漫画.js
+++ sources/多多漫画.js
@@ -5,21 +5,21 @@ function manifest() {
 		
 		//@NonNull 搜索源ID标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
-		id: 1652776303,
+		id: 1654843272,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
 		minMyACG: 20211219,
 		
 		//优先级1~100，数值越大越靠前
 		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
-		priority: 1,
+		priority: 20,
 		
 		//是否失效，默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
 		invalid: false,
 		
 		//@NonNull 搜索源名称
-		name: "二次元动漫",
+		name: "多多漫画",
 
 		//搜索源制作人
 		author: "雨夏",
@@ -28,19 +28,19 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 2,
+		version: 1,
 
 		//搜索源自动同步更新链接
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/二次元动漫.js",
-			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/二次元动漫.js",
-			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/二次元动漫.js",
-			"Coding": "https://ylk2534246654.coding.net/p/myacg/d/MyACGSourceRepository/git/raw/master/sources/二次元动漫.js",
-			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/二次元动漫.js",
+			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/多多漫画.js",
+			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/多多漫画.js",
+			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/多多漫画.js",
+			"Coding": "https://ylk2534246654.coding.net/p/myacg/d/MyACGSourceRepository/git/raw/master/sources/多多漫画.js",
+			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/多多漫画.js",
 		},
 		
 		//更新时间
-		updateTime: "2022年6月11日",
+		updateTime: "2022年3月29日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
@@ -52,10 +52,10 @@ function manifest() {
 		tag: ["漫画"],
 		
 		//@NonNull 详细界面的基本网址
-		baseUrl: "https://www.2animx.com",
+		baseUrl: "https://m.duoduomh.com",
 	});
 }
-const header = '';
+const header = '@ip->163.197.43.177';
 
 /**
  * 搜索
@@ -63,25 +63,54 @@ const header = '';
  * @returns {[{title, summary, cover, url}]}
  */
 function search(key) {
-	var url = 'http://www.2animx.com/search-index?&searchType=1&q=' + encodeURI(key) + header;
+	var url = 'https://m.duoduomh.com/search/?keywords=' + encodeURI(key) + header;
 	const response = httpRequest(url);
 	
-	const list = jsoupArray(response,'ul.liemh.htmls.indliemh > li').outerHtml();
+	const list = jsoupArray(response,'#update_list > div > div').outerHtml();
 	var array= [];
 	for (var i=0;i<list.length;i++) {
 	    var data = list[i];
 		array.push({
 			//标题
-			title : jsoup(data,'div[class=tit]').text(),
+			title : jsoup(data,'div.itemTxt > a').text(),
 			
 			//概览
-			summary : jsoup(data,'a > em').text(),
+			summary : jsoup(data,'a.coll').text(),
 			
 			//封面
-			cover : ToolUtil.urlJoin(url,jsoup(data,'img').attr('src')),
+			cover : jsoup(data,'div.itemImg > a > img').attr('src'),
 			
 			//链接
-			url : ToolUtil.urlJoin(url,jsoup(data,'a').attr('href'))
+			url : ToolUtil.urlJoin(url,jsoup(data,'div.itemTxt > a').attr('href'))
+			});
+	}
+	return JSON.stringify(array);
+}
+/**
+ * 搜索
+ * @params {string} key
+ * @returns {[{title, summary, cover, url}]}
+ 
+function search(key) {
+	var url = 'https://api.duoduomh.com/comic/search?page=1@post->keywords=' + encodeURI(key) + header;
+	const response = httpRequest(url);
+	
+	const list = jsonPathArray(response,'$.items.*');
+	var array= [];
+	for (var i=0;i<list.length;i++) {
+	    var data = list[i];
+		array.push({
+			//标题
+			title : jsonPath(data,'$.name'),
+			
+			//概览
+			summary : jsonPath(data,'$.author') + jsonPath(data,'$.last_chapter_name'),
+			
+			//封面
+			cover : jsonPath(data,'$.coverUrl'),
+			
+			//链接
+			url : 'https://m.duoduomh.com/manhua/'+jsonPath(data,'$.slug')
 			});
 	}
 	return JSON.stringify(array);
@@ -95,13 +124,16 @@ function detail(url) {
 	const response = httpRequest(url+ header);
 	return JSON.stringify({
 		//作者
-		author: jsoup(response,'dl.mh-detail > dd > p:nth-child(1) > a').text(),
+		author: jsoup(response,'div.sub_r > p:nth-child(1)').text(),
 		
 		//概览
-		summary: jsoup(response,'div.mh-introduce > p:nth-child(3)').text(),
+		summary: jsoup(response,'p.txtDesc#full-des').text(),
 
 		//封面
-		cover : ToolUtil.urlJoin(url,jsoup(response,'.mh-detail > dt > a >img').attr('src')),
+		cover : jsoup(response,'div.img > mip-img').attr('src'),
+
+		//更新时间
+		upDate: jsoup(response,'div.comic-view.clearfix > div.view-sub.autoHeight > div > dl:nth-child(5) > dd').text(),
 		
 		//目录是否倒序
 		reverseOrder: false,
@@ -125,16 +157,15 @@ function catalog(response,url) {
 	var newchapters= [];
 	
 	//章节代码
-	var chapters = jsoupArray(response,'#oneCon1 > ul > li').outerHtml();
+	var chapters = jsoupArray(response,'div.chapter-warp > ul > li').outerHtml();
 	
 	for (var ci=0;ci<chapters.length;ci++) {
 		var chapter = chapters[ci];
-		
 		newchapters.push({
 			//章节名称
 			name: jsoup(chapter,'a').text(),
 			//章节链接
-			url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href') + '-p-${p}@zero->1@start->1' +  header)
+			url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href').replace('.html','-${p}.html@zero->1@start->1'))
 		});
 	}
 	//添加目录
@@ -146,27 +177,17 @@ function catalog(response,url) {
 		});
 	return new_catalogs
 }
+
 /**
  * 内容
  * @params {string} url
  * @returns {[{url}]}
  */
- function content(url) {
+function content(url) {
 	const response = httpRequest(url + header);
-	return JSON.stringify(jsoup(response,'img[id = ComicPic]').attr('src'));
-}
-/**
- * 内容
- * @params {string} url
- * @returns {[{url}]}
- */
-function content2(url) {//比较耗时的单线程解析方法
-	var response = httpRequest(url + header);
-	const endid = jsoup(response,'#total').attr('value');
-	var obj= [];
-	for(var i =1;i <= endid;i++){
-		response = httpRequest(url + '-p-' + i + header);
-		obj.push(jsoup(response,'img[id = ComicPic]').attr('src'));
+	const src = jsoup(response,'div.image-content > img').attr('src');
+	if(src.indexOf('default') == -1){
+		return JSON.stringify(src);
 	}
-	return JSON.stringify(obj);
+	return null;
 }
