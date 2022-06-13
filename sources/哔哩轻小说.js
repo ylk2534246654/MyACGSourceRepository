1f1d6bfd92b740dbdeec58ff1b40738854e67d02
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2022-06-13 23:39:39 +0800

    Add files via upload

--- sources/哔哩轻小说.js
+++ sources/哔哩轻小说.js
@@ -56,7 +56,7 @@ function manifest() {
 	});
 }
 
-const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
+const header = '';
 /**
  * 搜索
  * @params {string} key
