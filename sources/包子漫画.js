c8c30c2c515543129a5e4a09f23b039b916daf81
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2022-12-06 16:33:50 +0800

    修复

--- sources/包子漫画.js
+++ sources/包子漫画.js
@@ -12,7 +12,7 @@ function manifest() {
 		
 		//优先级1~100，数值越大越靠前
 		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
-		priority: 1,//加载速度慢
+		priority: 10,//加载速度慢
 		
 		//是否失效，默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
@@ -28,32 +28,31 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 2,
+		version: 3,
 
 		//搜索源自动同步更新网址
 		syncList: {
 			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/包子漫画.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/包子漫画.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/包子漫画.js",
-			"Coding": "https://ylk2534246654.coding.net/p/myacg/d/MyACGSourceRepository/git/raw/master/sources/包子漫画.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/包子漫画.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/包子漫画.js",
 		},
 		
 		//更新时间
-		updateTime: "2022年7月27日",
+		updateTime: "2022年12月6日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
 		
 		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 2,
+		contentType: 1,
 		
 		//自定义标签
 		tag: ["漫画"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "https://cn.baozimh.com",
+		baseUrl: "https://cn.baozimh.com",//https://cn.webmota.com/
 	});
 }
 const header = '';
@@ -141,6 +140,21 @@ function catalog(response,url) {
 			url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
 		});
 	}
+	if(newchapters.length < 1){//最新章节
+		//章节代码
+		var chapters2 = jsoupArray(response,'.comics-chapters').outerHtml();
+		
+		for (var ci = chapters2.length;ci > 0;ci--) {
+			var chapter = chapters2[ci];
+			
+			newchapters.push({
+				//章节名称
+				name: jsoup(chapter,'a').text(),
+				//章节网址
+				url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
+			});
+		}
+	}
 	//添加目录
 	new_catalogs.push({
 		//目录名称
@@ -154,7 +168,7 @@ function catalog(response,url) {
  * 内容(InterceptRequest)
  * @params {string} url
  * @returns {string} content
- */
+
 function content(url) {
 	return url;
 }
@@ -163,12 +177,12 @@ function content(url) {
  * @params {string} url
  * @returns {string} content
  */
-function content1(url) {
+function content(url) {
 	const response = httpRequest(url + header);
 	//创建漫画数组
 	var chapterImages= [];
 	//漫画列表代码
-	var contents = jsoupArray(response,'div.chapter-main > ul > [src]').outerHtml();
+	var contents = jsoupArray(response,'[[src]],.comic-contain__item').outerHtml();
 	for (var ci=0;ci<contents.length;ci++) {
 		var content = contents[ci];
 		chapterImages.push(jsoup(content,'[src]').attr('src'));
