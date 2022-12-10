e45ba5e97b42bd02ce806a5f74d605e3f7a3e4ef
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2022-12-10 20:15:04 +0800

    新增

---
+++ sources/啵乐漫画.js
@@ -0,0 +1,167 @@
+function manifest() {
+	return JSON.stringify({
+		//MyACG 最新版本
+		MyACG: 'https://lanzou.com/b07xqlbxc ',
+		
+		//@NonNull 搜索源 ID 标识，设置后不建议更改
+		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
+		id: 1670670509,
+		
+		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
+		minMyACG: 20211219,
+		
+		//优先级1~100，数值越大越靠前
+		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		priority: 10,
+		
+		//是否失效，默认关闭
+		//true: 无法安装，并且已安装的变灰，用于解决失效源
+		invalid: false,
+		
+		//@NonNull 搜索源名称
+		name: "啵乐漫画",
+
+		//搜索源制作人
+		author: "雨夏",
+
+		//电子邮箱
+		email: "2534246654@qq.com",
+
+		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
+		version: 1,
+
+		//搜索源自动同步更新网址
+		syncList: {
+			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/啵乐漫画.js",
+			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/啵乐漫画.js",
+			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/啵乐漫画.js",
+			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/啵乐漫画.js",
+			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/啵乐漫画.js",
+		},
+		
+		//更新时间
+		updateTime: "2022年12月10日",
+		
+		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
+		type: 2,
+		
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
+		contentType: 1,
+		
+		//自定义标签
+		tag: ["漫画"],
+		
+		//@NonNull 详情页的基本网址
+		baseUrl: "http://www.bolebl.com",//备用：http://www.404ms.top
+	});
+}
+const header = '';
+
+/**
+ * 搜索
+ * @params {string} key
+ * @returns {[{title, summary, cover, url}]}
+ */
+function search(key) {
+	var url = 'http://www.bolebl.com/search?keyword=' + encodeURI(key) + header;
+	const response = httpRequest(url);
+	
+	const list = jsoupArray(response,'.acgn-item').outerHtml();
+	var array= [];
+	for (var i=0;i<list.length;i++) {
+	    var data = list[i];
+		array.push({
+			//标题
+			title : jsoup(data,'.acgn-title').text(),
+			
+			//概览
+			summary : jsoup(data,'.acgn-desc').text(),
+			
+			//封面
+			cover : ToolUtil.urlJoin(url,ToolUtil.substring(jsoup(data,'img').attr('style'),'url(\'','\'')),
+			
+			//网址
+			url : ToolUtil.urlJoin(url,jsoup(data,'.acgn-thumbnail').attr('href'))
+			});
+	}
+	return JSON.stringify(array);
+}
+/**
+ * 详情
+ * @params {string} url
+ * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
+ */
+function detail(url) {
+	const response = httpRequest(url+ header);
+	return JSON.stringify({
+		//标题
+		title : jsoup(response,'div.content > h1').text(),
+		
+		//作者
+		author: jsoup(response,'div.content > p > span:nth-child(4)').text(),
+		
+		//日期
+		date : jsoup(response,'.update').text(),
+		
+		//概览
+		summary: jsoup(response,'.desc-content').text(),
+
+		//封面
+		cover : ToolUtil.urlJoin(url,ToolUtil.substring(jsoup(response,'div.detail-cover > img').attr('style'),'url(\'','\'')),
+		
+		//目录是否倒序
+		reverseOrder: false,
+		
+		//目录加载
+		catalog: catalog(response,url)
+	})
+}
+
+/**
+ * 目录
+ * @params {string} response
+ * @params {string} url
+ * @returns {[{tag, chapter:{[{name, url}]}}]}
+ */
+function catalog(response,url) {
+	//创建目录数组
+	var new_catalogs= [];
+		
+	//创建章节数组
+	var newchapters= [];
+		
+	//章节代码
+	var chapters = jsoupArray(response,'.chapter-list > li').outerHtml();
+		
+	for (var ci=0;ci<chapters.length;ci++) {
+		var chapter = chapters[ci];
+			
+		newchapters.push({
+			//章节名称
+			name: jsoup(chapter,'a').text(),
+			//章节网址
+			url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
+		});
+	}
+	//添加目录
+	new_catalogs.push({
+		//目录名称
+		tag: '目录',
+		//章节
+		chapter : newchapters
+	});
+	return new_catalogs;
+}
+
+/**
+ * 内容（部分漫画搜索源通用规则）
+ * @version 2022/12/8
+ * 168,思思，39 , 360 , 147 , 动漫画 ，依依
+ * @params {string} url
+ * @returns {string} content
+ */
+function content(url) {
+	const response = httpRequest(url + header);
+	var imgList =  jsoupArray(response,'img[referrerpolicy]').attr('src');
+	return JSON.stringify(imgList);
+}
