7359fbb049d0d467a97651de1dfc735581802342
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-01-22 22:11:38 +0800

    兼容新版

--- sources/dmh8樱花动漫.js
+++ sources/dmh8樱花动漫.js
@@ -1,16 +1,14 @@
-const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 iPhone';
-
 function manifest() {
 	return JSON.stringify({
 		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
+		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
 		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1654704919,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220705,
+		minMyACG: 20230122,
 
 		//优先级1~100，数值越大越靠前
 		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
@@ -30,20 +28,19 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 4,
+		version: 7,
 
 		//搜索源自动同步更新网址
 		syncList: {
 			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/dmh8樱花动漫.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/dmh8樱花动漫.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/dmh8樱花动漫.js",
-			"Coding": "https://ylk2534246654.coding.net/p/myacg/d/MyACGSourceRepository/git/raw/master/sources/dmh8樱花动漫.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/dmh8樱花动漫.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/dmh8樱花动漫.js",
 		},
 		
 		//更新时间
-		updateTime: "2022年11月23日",
+		updateTime: "2023年1月22日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
@@ -55,7 +52,7 @@ function manifest() {
 		tag: ["动漫"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "http://www.dmh8.me",//和哆咪动漫相似
+		baseUrl: baseUrl,
 		
 		//登录授权是否启用
 		auth: false,
@@ -67,7 +64,8 @@ function manifest() {
 		authRequired: ["search"],
 	});
 }
-
+const baseUrl = "http://www.dmh8.me";//和哆咪动漫类似
+const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 iPhone';
 /*
  * 拦截并验证手动授权数据
  * @params {string} html	网页源码
@@ -98,28 +96,29 @@ function authVerify() {
  * @returns {[{title, summary, cover, url}]}
  */
 function search(key) {
-	var url = 'http://www.dmh8.me/search.asp?searchword='+ encodeURI(key) + header;
+	var url = ToolUtil.urlJoin(baseUrl,'/search.asp?searchword=' + encodeURI(key) + header);
 	const response = httpRequest(url);
 	
-	const list = jsoupArray(response,'#searchList > li').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
+	var result= [];
+    var document = org.jsoup.Jsoup.parse(response,baseUrl);
+    var elements = document.select("#searchList > li");
+	for (var i = 0;i < elements.size();i++) {
+	    var element = elements.get(i);
+		result.push({
 			//标题
-			title : jsoup(data,'.title').text(),
+			title: element.selectFirst('.title').text(),
 			
 			//概览
-			summary : jsoup(data,'div.detail > p.hidden-xs > :matchText').text(),
+			summary: element.selectFirst('div.detail > p.hidden-xs > :matchText').text(),
 			
 			//封面
-			cover : ToolUtil.urlJoin(url,jsoup(data,'div.thumb > a').attr('data-original')),
+			cover: element.selectFirst('div.thumb > a').absUrl('data-original'),
 			
 			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'.title > a').attr('href'))
-			});
+			url: element.selectFirst('.title > a').absUrl('href')
+		});
 	}
-	return JSON.stringify(array);
+	return JSON.stringify(result);
 }
 
 /**
@@ -129,101 +128,97 @@ function search(key) {
  */
 function find(url) {
 	const response = httpRequest(url + header);
-	//目录标签代码
-	const list = jsoupArray(response,'div.list > ul > li').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
+	
+	var result = [];
+	var document = org.jsoup.Jsoup.parse(response,baseUrl);
+	var elements = document.select("div.list > ul > li");
+	for (var i = 0;i < elements.size();i++) {
+	    var element = elements.get(i);
+		result.push({
 			//标题
-			title : jsoup(data,'a.itemtext').text(),
+			title: element.selectFirst('a.itemtext').text(),
 			
 			//概览
-			summary : jsoup(data,'div:nth-child(7) > span.cell_imform_value').text(),
+			summary: element.selectFirst('div:nth-child(7) > span.cell_imform_value').text(),
 			
 			//封面
-			cover : ToolUtil.urlJoin(url,ToolUtil.substring(jsoup(data,'div.imgblock').attr('style'),'\'','\'')),
+			cover: ToolUtil.urlJoin(url,ToolUtil.substring(element.selectFirst('div.imgblock').attr('style'),'\'','\'')),
 			
 			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'a.itemtext').attr('href'))
-			});
+			url: element.selectFirst('a.itemtext').absUrl('href')
+		});
 	}
-	return JSON.stringify(array);
+	return JSON.stringify(result);
 }
 /**
  * 详情
  * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
+ * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = httpRequest(url+ header);
+	const response = httpRequest(url + header);
+    var document = org.jsoup.Jsoup.parse(response,baseUrl);
 	return JSON.stringify({
 		//标题
-		title : jsoup(response,'h1.title').text(),
-
-		//作者
-		author: jsoup(response,'div.myui-content__detail > p:nth-child(7) > a').text(),
+		title: document.selectFirst('h1.title').text(),
 		
-		//更新时间
-		date: jsoup(response,'p.data > a:nth-child(8)').text(),
+		//作者
+		author: document.selectFirst('div.myui-content__detail > p:nth-child(7) > a').text(),
+		
+		//日期
+		date: document.selectFirst('p.data > a:nth-child(8)').text(),
 		
 		//概览
-		summary: jsoup(response,'div.content').text(),
+		summary: document.selectFirst('div.content').text(),
 
 		//封面
-		cover : jsoup(response,'div.myui-content__thumb > a > img').attr('data-original'),
+		cover: document.selectFirst('div.myui-content__thumb > a > img').absUrl('data-original'),
 		
 		//目录是否倒序
 		reverseOrder: false,
 		
-		//目录网址/非外链无需使用
-		catalog: catalog(response,url)
-	})
+		//目录加载
+		catalogs: catalogs(document)
+	});
 }
 /**
  * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
+ * @returns {[{name, chapters:{[{name, url}]}}]}
  */
-function catalog(response,url) {
-	//目录标签代码
-	const tabs = jsoupArray(response,'ul.nav-tabs > li').outerHtml();
+function catalogs(document) {
+	const tagElements = document.select('ul.nav-tabs > li');
 	
-	//目录代码
-	const catalogs = jsoupArray(response,'div.tab-pane').outerHtml();
+	//目录元素选择器
+	const catalogElements= document.select('div.tab-pane');
 	
 	//创建目录数组
-	var new_catalogs= [];
+	var newCatalogs = [];
 	
-	for (var i=0;i<catalogs.length;i++) {
-	    var catalog = catalogs[i];
-		
+	for (var i = 0;i < catalogElements.size();i++) {
 		//创建章节数组
-		var newchapters= [];
+		var newChapters = [];
 		
-		//章节代码
-		var chapters = jsoupArray(catalog,'ul > li').outerHtml();
+		//章节元素选择器
+		var chapterElements = catalogElements.get(i).select('ul > li');
 		
-		for (var ci=0;ci<chapters.length;ci++) {
-			var chapter = chapters[ci];
+		for (var i2 = 0;i2 < chapterElements.size();i2++) {
+			var chapterElement = chapterElements.get(i2);
 			
-			newchapters.push({
+			newChapters.push({
 				//章节名称
-				name: jsoup(chapter,'a').text(),
+				name: chapterElement.selectFirst('a').text(),
 				//章节网址
-				url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href')) + header
+				url: chapterElement.selectFirst('a').absUrl('href') + header
 			});
 		}
-		//添加目录
-		new_catalogs.push({
+		newCatalogs.push({
 			//目录名称
-			tag: jsoup(tabs[i],'li').text(),
+			name: tagElements.get(i).selectFirst('li').text(),
 			//章节
-			chapter : newchapters
-			});
+			chapters : newChapters
+		});
 	}
-	return new_catalogs
+	return newCatalogs;
 }
 /**
  * 内容（部分动漫搜索源通用规则）
@@ -231,8 +226,14 @@ function catalog(response,url) {
  * @returns {string} content
  */
 function content(url) {
-	//浏览器请求结果处理，嘻嘻动漫，12wo动漫，路漫漫，风车动漫P，樱花动漫P 相似
-	var re = /([a-z]+:\/\/[a-z]*.[a-z]*.[a-z]*\/[a-z]\/\d)|(\?_r=)/i;
+	//浏览器请求结果处理，布米米，嘻嘻动漫，12wo动漫，路漫漫，风车动漫P，樱花动漫P，dmh8樱花动漫 相似
+	var re = /[a-z]+:\/\/[\w.]+\/([a-z]{1}\/\d|[a-z]{3}\/[a-z]{3}\/\d|[a-z]{2}\/\d{4}\?)/i;
+	
+	//这种格式均为广告网址
+	//https://knr.xxxxx.cn/j/140000		#[a-z]{1}\/\d
+	//https://xx.xxx.xx/xxx/xxx/0000	#[a-z]{3}\/[a-z]+\/\d
+	//https://tg.xxx.com/sc/0000?n=xxxx #[a-z]{2}\/\d{4}\?
+	
 	if(!re.test(url)){
 		return url;
 	}
