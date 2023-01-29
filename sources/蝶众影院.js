0d809a53e96fd6e77345fc420b91a96b37755de1
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-01-29 16:54:41 +0800

    新增

---
+++ sources/蝶众影院.js
@@ -0,0 +1,175 @@
+function manifest() {
+	return JSON.stringify({
+		//MyACG 最新版本
+		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
+		
+		//@NonNull 搜索源 ID 标识，设置后不建议更改
+		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
+		id: 1674978871,
+		
+		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
+		minMyACG: 20230122,
+
+		//优先级1~100，数值越大越靠前
+		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		priority: 1,
+		
+		//是否失效，默认关闭
+		//true: 无法安装，并且已安装的变灰，用于解决失效源
+		invalid: false,
+		
+		//@NonNull 搜索源名称
+		name: "蝶众影院",
+
+		//搜索源制作人
+		author: "雨夏",
+
+		//电子邮箱
+		email: "2534246654@qq.com",
+
+		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
+		version: 1,
+
+		//搜索源自动同步更新链接
+		syncList: {
+			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/蝶众影院.js",
+			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/蝶众影院.js",
+			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/蝶众影院.js",
+			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/蝶众影院.js",
+			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/蝶众影院.js",
+		},
+		
+		//更新时间
+		updateTime: "2023年1月29日",
+		
+		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
+		type: 3,
+		
+		//内容处理方式： -1: 搜索相似，0：对链接处理并调用外部APP访问，1：对链接处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
+		contentType: 2,
+		
+		//自定义标签
+		tag: ["影视"],
+		
+		//@NonNull 详情页的基本网址
+		baseUrl: baseUrl,
+	});
+}
+
+const baseUrl = "https://www.diezz.net";
+const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
+
+/**
+ * 搜索
+ * @params {string} key
+ * @returns {[{title, summary, cover, url}]}
+ */
+function search(key) {
+	var url = ToolUtil.urlJoin(baseUrl,'/vodsearch/-------------.html?wd='+ encodeURI(key) + header);
+	const response = httpRequest(url);
+	
+	var result = [];
+    var document = org.jsoup.Jsoup.parse(response,url);
+    var elements = document.select(".searchlist_item");
+	for (var i = 0;i < elements.size();i++) {
+	    var element = elements.get(i);
+		result.push({
+			//标题
+			title: element.selectFirst('.vodlist_title > a > :matchText').text(),
+			
+			//概览
+			summary: element.selectFirst('.pic_text').text(),
+			
+			//封面
+			cover: element.selectFirst('.searchlist_img > a').absUrl('data-original'),
+			
+			//网址
+			url: element.selectFirst('.searchlist_img > a').absUrl('href')
+		});
+	}
+	return JSON.stringify(result);
+}
+/**
+ * 详情
+ * @params {string} url
+ * @returns {[{title, author, date, summary, cover, reverseOrder, catalogs:{[{name, chapters:{[{name, url}]}}]}}]}
+ */
+function detail(url) {
+	const response = httpRequest(url + header);
+    var document = org.jsoup.Jsoup.parse(response,url);
+	return JSON.stringify({
+		//标题
+		title: document.selectFirst('.title').text(),
+		
+		//作者
+		//author: document.selectFirst('').text(),
+		
+		//日期
+		date: document.selectFirst('.content_detail > ul > li:nth-child(2) > em').text(),
+		
+		//概览
+		summary: document.selectFirst('.full_text > span').text(),
+
+		//封面
+		cover: document.selectFirst('.content_thumb > a').absUrl('data-original'),
+		
+		//目录是否倒序
+		reverseOrder: false,
+		
+		//目录加载
+		catalogs: catalogs(document)
+	});
+}
+
+/**
+ * 目录
+ * @returns {[{name, chapters:{[{name, url}]}}]}
+ */
+function catalogs(document) {
+	const tagElements = document.select('.play_source_tab > a');
+	
+	//目录元素选择器
+	const catalogElements= document.select('.play_list_box > .playlist_full');
+	
+	//创建目录数组
+	var newCatalogs = [];
+	
+	for (var i = 0;i < catalogElements.size();i++) {
+		//创建章节数组
+		var newChapters = [];
+		
+		//章节元素选择器
+		var chapterElements = catalogElements.get(i).select('ul > li');
+		
+		for (var i2 = 0;i2 < chapterElements.size();i2++) {
+			var chapterElement = chapterElements.get(i2);
+			
+			newChapters.push({
+				//章节名称
+				name: chapterElement.selectFirst('a').text(),
+				//章节网址
+				url: chapterElement.selectFirst('a').absUrl('href')
+			});
+		}
+		newCatalogs.push({
+			//目录名称
+			name: tagElements.get(i).attr('alt'),
+			//章节
+			chapters: newChapters
+		});
+	}
+	return newCatalogs
+}
+/**
+ * 内容(InterceptRequest)
+ * @params {string} url
+ * @returns {string} content
+ */
+function content(url) {
+	//https://js.tydouke.com/bid?url=
+	//展示图片：https://abb.juntaijiancai.com/file/creative/2022/10/04/6627347.gif.oef
+	if(url.indexOf('tydouke.com') == -1){
+		return url;
+	}
+	return null;
+}
