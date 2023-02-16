170da3a56811f3daa38ebb5ff20bd75dcea38dc1
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-02-16 10:48:25 +0800

    优化搜索源

--- sources/哔咪动漫.js
+++ sources/哔咪动漫.js
@@ -1,14 +1,14 @@
 function manifest() {
 	return JSON.stringify({
 		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
+		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
 		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1655214571,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220101,
+		minMyACG: 20230207,
 
 		//优先级1~100，数值越大越靠前
 		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
@@ -28,20 +28,19 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 2,
+		version: 4,
 
 		//搜索源自动同步更新网址
 		syncList: {
 			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/哔咪动漫.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/哔咪动漫.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/哔咪动漫.js",
-			"Coding": "https://ylk2534246654.coding.net/p/myacg/d/MyACGSourceRepository/git/raw/master/sources/哔咪动漫.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/哔咪动漫.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/哔咪动漫.js",
 		},
 		
 		//更新时间
-		updateTime: "2022年7月15日",
+		updateTime: "2023年2月16日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
@@ -53,159 +52,159 @@ function manifest() {
 		tag: ["动漫"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "http://bimiacg4.net",//http://bimiacg.one/
-		
+		baseUrl: baseUrl,
 		
 		//发现
 		findList: {
-			"最近更新": "http://bimiacg4.net/type/riman/"
+			"最近更新": ToolUtil.urlJoin(baseUrl,"/type/riman/")
 		},
 	});
 }
+const baseUrl = "http://bimiacg4.net";//http://bimiacg.one/
 const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
 
 /**
  * 搜索
- * @params {string} key
- * @returns {[{title, summary, cover, url}]}
+ * @param {string} key
+ * @returns {[{title, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = 'http://bimiacg4.net/vod/search/@post->wd='+ encodeURI(key) + header;
+	var url = ToolUtil.urlJoin(baseUrl,'/vod/search/@post->wd='+ encodeURI(key) + header);
 	const response = httpRequest(url);
 	
-	const list = jsoupArray(response,'ul.tab-cont > li').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
+	var result = [];
+    var document = org.jsoup.Jsoup.parse(response,url);
+	var elements = document.select("ul.tab-cont > li");
+	for (var i = 0;i < elements.size();i++) {
+	    var element = elements.get(i);
+		result.push({
 			//标题
-			title : jsoup(data,'div.info > a').text(),
+			title: element.selectFirst('div.info > a').text(),
 			
 			//概览
-			summary : jsoup(data,'div.info > p').text(),
+			summary: element.selectFirst('div.info > p').text(),
 			
-			//封面
-			cover : ToolUtil.urlJoin(url,jsoup(data,'img.lazy').attr('data-original')),
+			//封面网址
+			coverUrl: element.selectFirst('img.lazy').absUrl('data-original'),
 			
 			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'div.info > a').attr('href'))
-			});
+			url: element.selectFirst('div.info > a').absUrl('href')
+		});
 	}
-	return JSON.stringify(array);
+	return JSON.stringify(result);
 }
 /**
  * 发现
- * @params string url
- * @returns {[{title, summary, cover, url}]}
+ * @param string url
+ * @returns {[{title, summary, coverUrl, url}]}
  */
 function find(url) {
 	const response = httpRequest(url + header);
-	//目录标签代码
-	const list = jsoupArray(response,'ul.tab-cont > li').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
+	
+	var result = [];
+    var document = org.jsoup.Jsoup.parse(response,url);
+	var elements = document.select("ul.tab-cont > li");
+	for (var i = 0;i < elements.size();i++) {
+	    var element = elements.get(i);
+		result.push({
 			//标题
-			title : jsoup(data,'div.info > a').text(),
+			title: element.selectFirst('div.info > a').text(),
 			
 			//概览
-			summary : jsoup(data,'div.info > p').text(),
+			summary: element.selectFirst('div.info > p').text(),
 			
-			//封面
-			cover : ToolUtil.urlJoin(url,jsoup(data,'img.lazy').attr('data-original')),
+			//封面网址
+			coverUrl: element.selectFirst('img.lazy').absUrl('data-original'),
 			
 			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'div.info > a').attr('href'))
-			});
+			url: element.selectFirst('div.info > a').absUrl('href')
+		});
 	}
-	return JSON.stringify(array);
+	return JSON.stringify(result);
 }
 
 /**
  * 详情
- * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
+ * @returns {[{title, author, date, summary, coverUrl, isReverseOrder, catalogs:{[{name, chapters:{[{name, url}]}}]}}]}
  */
 function detail(url) {
 	const response = httpRequest(url+ header);
+	var document = org.jsoup.Jsoup.parse(response,url);
 	return JSON.stringify({
 		//标题
-		title : jsoup(response,'div.tit > h1').text(),
+		title: document.selectFirst('div.tit > h1').text(),
 		
 		//作者
-		author: jsoup(response,'div.txt_intro_con > ul > li:nth-child(4) > storng > a').text(),
+		author: document.selectFirst('div.txt_intro_con > ul > li:nth-child(4) > storng > a').text(),
 		
 		//日期
-		date : jsoup(response,'div.txt_intro_con > ul > li:nth-child(10) > :matchText').text(),
+		date: document.selectFirst('div.txt_intro_con > ul > li:nth-child(10) > :matchText').text(),
 		
 		//概览
-		summary: jsoup(response,'li.li_intro > p:nth-child(3)').text(),
+		summary: document.selectFirst('li.li_intro > p:nth-child(3)').text(),
 
-		//封面
-		cover : jsoup(response,'.v_pic > img.lazy').attr('src'),
+		//封面网址
+		coverUrl: document.selectFirst('.v_pic > img.lazy').absUrl('src'),
 		
 		//目录是否倒序
-		reverseOrder: false,
+		isReverseOrder: false,
 		
-		//目录网址/非外链无需使用
-		catalog: catalog(response,url)
-	})
+		//目录加载
+		catalogs: catalogs(document)
+	});
 }
+
 /**
  * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
+ * @returns {[{name, chapters:{[{name, url}]}}]}
  */
-function catalog(response,url) {
-	//目录标签代码
-	const tabs = jsoupArray(response,'.play_source_tab > a').outerHtml();
+function catalogs(document) {
+	const tagElements = document.select('.play_source_tab > a');
 	
-	//目录代码
-	const catalogs = jsoupArray(response,'.player_list').outerHtml();
+	//目录元素选择器
+	const catalogElements= document.select('.player_list');
 	
 	//创建目录数组
-	var new_catalogs= [];
+	var newCatalogs = [];
 	
-	for (var i=0;i<catalogs.length;i++) {
-	    var catalog = catalogs[i];
-		
+	for (var i = 0;i < catalogElements.size();i++) {
 		//创建章节数组
-		var newchapters= [];
+		var newChapters = [];
 		
-		//章节代码
-		var chapters = jsoupArray(catalog,'ul > li').outerHtml();
+		//章节元素选择器
+		var chapterElements = catalogElements.get(i).select('ul > li');
 		
-		for (var ci=0;ci<chapters.length;ci++) {
-			var chapter = chapters[ci];
+		for (var i2 = 0;i2 < chapterElements.size();i2++) {
+			var chapterElement = chapterElements.get(i2);
 			
-			newchapters.push({
+			newChapters.push({
 				//章节名称
-				name: jsoup(chapter,'a').text(),
+				name: chapterElement.selectFirst('a').text(),
 				//章节网址
-				url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
+				url: chapterElement.selectFirst('a').absUrl('href')
 			});
 		}
-		//添加目录
-		new_catalogs.push({
+		newCatalogs.push({
 			//目录名称
-			tag: jsoup(tabs[i],'a').text(),
+			name: tagElements.get(i).selectFirst('a').text(),
 			//章节
-			chapter : newchapters
-			});
+			chapters: newChapters
+		});
 	}
-	return new_catalogs
+	return newCatalogs
 }
 
 /**
  * 内容(InterceptRequest)
- * @params {string} url
  * @returns {string} content
  */
 function content(url) {
 	//浏览器请求结果处理
-	var re = /sohu|hm\.|\.gov|\.qq|\.alpha|cpv|360buyimg|suning|knmer|qqmail_head|adInnovationResource/i;
+	var re = /sohu|hm\.|\.gov|\.qq|\.alpha|cpv|360buyimg|suning|knmer|qqmail_head|adInnovationResource|[a-z]+:\/\/[\w.]+\/[a-z]{1}\/[a-z]{1}\?/i;
+	
+	//这种格式均为广告网址
+	//https://api.simi0000.com/s/a?_=000000000000000000
+
 	if(!re.test(url)){
 		return url;
 	}
