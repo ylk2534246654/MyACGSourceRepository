8eb17390cf0a62fab0ec51936c7656668993c070
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-03-15 22:28:12 +0800

    修复并不再兼容 1.4.3 以下版本

--- sources/90漫画.js
+++ sources/90漫画.js
@@ -8,7 +8,7 @@ function manifest() {
 		id: 1652586404,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20230207,
+		minMyACG: 20230315,
 		
 		//优先级1~100，数值越大越靠前
 		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
@@ -16,7 +16,7 @@ function manifest() {
 		
 		//是否失效，默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isInvalid: false,
+		isEnabledInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "90漫画",
@@ -28,7 +28,7 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 3,
+		version: 4,
 
 		//搜索源自动同步更新网址
 		syncList: {
@@ -40,7 +40,7 @@ function manifest() {
 		},
 		
 		//更新时间
-		updateTime: "2023年1月7日",
+		updateTime: "2023年3月15日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
@@ -49,7 +49,7 @@ function manifest() {
 		contentType: 1,
 		
 		//自定义标签
-		group: ["漫画"],
+		groupName: ["漫画"],
 		
 		//@NonNull 详情页的基本网址
 		baseUrl: baseUrl,
@@ -60,72 +60,76 @@ const header = '';
 
 /**
  * 搜索
- * @params {string} key
+ * @param {string} key
  * @returns {[{title, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = ToolUtil.urlJoin(baseUrl,'/app/comic/search?sort=click&keywords=' + encodeURI(key) + header);
-	var response = httpRequest(url);
-	
-	var array= [];
-	const $ = JSON.parse(response)
-	$.items.forEach((child) => {
-		array.push({
-			//标题
-			title: child.name,
-	
-			//概览
-			summary: child.last_chapter_name,
-	
-			//封面网址
-			coverUrl: ToolUtil.urlJoin('https://js.tingliu.cc/',child.cover),
-	
-			//网址
-			url: ToolUtil.urlJoin(baseUrl,'/app/comic/view?id=' + child.id)
+	var url = ToolUtils.urlJoin(baseUrl,'/app/comic/search?sort=click&keywords=' + encodeURI(key) + header);
+	var response = HttpRequest(url);
+	var result= [];
+	if(response.code() == 200){
+		const $ = JSON.parse(response.html())
+		$.items.forEach((child) => {
+			result.push({
+				//标题
+				title: child.name,
+		
+				//概览
+				summary: child.last_chapter_name,
+		
+				//封面网址
+				coverUrl: ToolUtils.urlJoin('https://js.tingliu.cc/',child.cover),
+		
+				//网址
+				url: ToolUtils.urlJoin(baseUrl,'/app/comic/view?id=' + child.id)
+			})
 		})
-	})
-	return JSON.stringify(array);
+	}
+	return JSON.stringify(result);
 }
 /**
  * 详情
- * @returns {[{title, author, date, summary, coverUrl, isReverseOrder, catalogs:{[{name, chapters:{[{name, url}]}}]}}]}
+ * @returns {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = httpRequest(url + header);
-	const $ = JSON.parse(response);
-	return JSON.stringify({
-		//标题
-		title : $.data.title,
-		
-		//作者
-		author: $.data.author,
-		
-		//日期
-		//date: ,
-		
-		//概览
-		summary: $.data.description,
+	const response = HttpRequest(url + header);
+	if(response.code() == 200){
+		const $ = JSON.parse(response.html());
+		return JSON.stringify({
+			//标题
+			title : $.data.title,
+			
+			//作者
+			author: $.data.author,
+			
+			//日期
+			//date: ,
+			
+			//概览
+			summary: $.data.description,
 
-		//封面网址
-		coverUrl: ToolUtil.urlJoin('https://js.tingliu.cc/',$.data.cover),
-		
-		//目录是否倒序
-		isReverseOrder: false,
-		
-		//目录加载
-		catalogs: catalogs(response)
-	})
+			//封面网址
+			coverUrl: ToolUtils.urlJoin('https://js.tingliu.cc/',$.data.cover),
+			
+			//目录是否倒序
+			isEnabledChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(response)
+		});
+	}
+	return null;
 }
 
 /**
  * 目录
  * @returns {[{name, chapters:{[{name, url}]}}]}
  */
-function catalogs(response) {
+function tocs(response) {
 	//创建章节数组
 	var newChapters= [];
 	
-	var chapters = com.jayway.jsonpath.JsonPath.parse(response).read('$.data.chapterGroup.*.*');
+	var chapters = com.jayway.jsonpath.JsonPath.parse(response.html()).read('$.data.chapterGroup.*.*');
 	const $ = JSON.parse(chapters)
 	$.forEach((child) => {
 		newChapters.push({
@@ -148,11 +152,14 @@ function catalogs(response) {
  * @returns {string} content
  */
 function content(url) {
-	const response = httpRequest(url + header);
-	const $ = JSON.parse(response);
-	var newImageArray= [];
-	$.data.imageArray.forEach((child) => {
-		newImageArray.push('http://js.tingliu.cc/' + $.data.path + child);
-	})
-	return JSON.stringify(newImageArray);
+	const response = HttpRequest(url + header);
+	if(response.code() == 200){
+		const $ = JSON.parse(response.html());
+		var newImageArray= [];
+		$.data.imageArray.forEach((child) => {
+			newImageArray.push('http://js.tingliu.cc/' + $.data.path + child);
+		})
+		return JSON.stringify(newImageArray);
+	}
+	return null;
 }
