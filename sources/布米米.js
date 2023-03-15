4bd9eb88439fcb47b51cca9a6984159857b44360
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-03-15 22:50:04 +0800

    修复并不再兼容 1.4.3 以下版本

--- sources/布米米.js
+++ sources/布米米.js
@@ -8,15 +8,15 @@ function manifest() {
 		id: 1654934193,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220101,
+		minMyACG: 20230315,
 
 		//优先级1~100，数值越大越靠前
 		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
 		priority: 60,
 		
-		//是否失效，默认关闭
+		//是否启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isInvalid: false,
+		isEnabledInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "布米米",
@@ -40,13 +40,16 @@ function manifest() {
 		},
 		
 		//更新时间
-		updateTime: "2023年2月22日",
+		updateTime: "2023年3月15日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
 		
 		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
 		contentType: 2,
+		
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
+		groupName: 2,
 		
 		//自定义标签
 		group: ["动漫","影视"],
@@ -64,115 +67,121 @@ const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) Ap
  */
 function search(key) {
 	var url = 'http://119.91.129.193:7899/sssv.php?top=10&q='+ encodeURI(key) + header + '@header->referer:http://bumimi.com/@header->Origin:http://bumimi.com';
-	const response = '[' + ToolUtil.substring(httpRequest(url),'[',']') + ']';
-	const list = jsonPathArray(response,'$.*');
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
-			//标题
-			title : jsonPath(data,'$.title'),
-			
-			//概览
-			summary : jsonPath(data,'$.lianzaijs'),
-			
-			//封面
-			cover : jsonPath(data,'$.thumb'),
-			
-			//网址
-			url : jsonPath(data,'$.url')
+	var response = HttpRequest(url);
+	var result = [];
+	if(response.code() == 200){
+		const $ = JSON.parse('[' + ToolUtils.substring(response.html(),'[',']') + ']');
+		$.forEach((child) => {
+			result.push({
+				//标题
+				title : child.title,
+				
+				//概览
+				summary : child.lianzaijs,
+				
+				//封面
+				cover : child.thumb,
+				
+				//网址
+				url : child.url
 			});
+		});
 	}
-	return JSON.stringify(array);
+	return JSON.stringify(result);
 }
 
 /**
  * 详情
- * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
+ * @returns {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = httpRequest(url+ header);
-	return JSON.stringify({
-		//标题
-		title : jsoup(response,'div.top-info-title > div > h1').text(),
-		
-		//作者
-		//author: jsoup(response,'li:nth-child(5) > span.detail_imform_value').text(),
-		
-		//概览
-		summary: jsoup(response,'.item-desc').text(),
+	const response = HttpRequest(url + header);
+	var result= [];
+	if(response.code() == 200){
+		var document = response.document();
+		return JSON.stringify({
+			//标题
+			title: document.selectFirst('div.top-info-title > div > h1').text(),
+			
+			//作者
+			//author: document.selectFirst('').text(),
+			
+			//日期
+			//update: document.selectFirst('').text(),
+			
+			//概览
+			summary: document.selectFirst('.item-desc').text(),
 
-		//封面
-		cover : jsoup(response,'.b-detailcover > img').attr('data-img'),
-		
-		//目录是否倒序
-		reverseOrder: true,
-		
-		//目录网址/非外链无需使用
-		catalog: catalog(response,url)
-	})
+			//封面网址
+			coverUrl: document.selectFirst('.b-detailcover > img').absUrl('data-img'),
+			
+			//目录是否倒序
+			isEnabledChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
 }
+
 /**
  * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
+ * @returns {[{name, chapters:{[{name, url}]}}]}
  */
-function catalog(response,url) {
-	//目录标签代码
-	const tabs = jsoupArray(response,'.playurl> li,.num-tab > h2').outerHtml();
+function tocs(document) {
+	const tagElements = document.select('.playurl> li,.num-tab > h2');
 	
-	//目录代码
-	const catalogs = jsoupArray(response,'#qiyi-pl-list > div > ul').outerHtml();
+	//目录元素选择器
+	const tocElements= document.select('#qiyi-pl-list > div > ul');
 	
 	//创建目录数组
-	var new_catalogs= [];
+	var newTocs = [];
 	
-	for (var i=0;i<catalogs.length;i++) {
-	    var catalog = catalogs[i];
-		
+	for (var i = 0;i < tocElements.size();i++) {
 		//创建章节数组
-		var newchapters= [];
+		var newChapters = [];
 		
-		//章节代码
-		var chapters = jsoupArray(catalog,'ul > li').outerHtml();
+		//章节元素选择器
+		var chapterElements = tocElements.get(i).select('ul > li');
 		
-		for (var ci=0;ci<chapters.length;ci++) {
-			var chapter = chapters[ci];
+		for (var i2 = 0;i2 < chapterElements.size();i2++) {
+			var chapterElement = chapterElements.get(i2);
 			
-			newchapters.push({
+			newChapters.push({
 				//章节名称
-				name: jsoup(chapter,'a').text(),
+				name: chapterElement.selectFirst('a').text(),
 				//章节网址
-				url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
+				url: chapterElement.selectFirst('a').absUrl('href')
 			});
 		}
-		//添加目录
-		new_catalogs.push({
+		newTocs.push({
 			//目录名称
-			tag: jsoup(tabs[i],'a').text(),
+			name: tagElements.get(i).selectFirst('a').text(),
 			//章节
-			chapter : newchapters
-			});
+			chapters: newChapters
+		});
 	}
-	return new_catalogs
+	return newTocs
 }
 
 /**
- * 内容（部分动漫搜索源通用规则）
- * @version 2023/2/22
- * 布米米、嘻嘻动漫、12wo动漫、路漫漫、风车动漫P、樱花动漫P
+ * 内容（部分搜索源通用规则）
+ * @version 2023/3/15
+ * 布米米、嘻嘻动漫、12wo动漫、路漫漫、风车动漫P、樱花动漫P、COCO漫画
  * @returns {string} content
  */
 function content(url) {
-	var re = /[a-z]+:\/\/[\w.]+\/([a-z]{1}\/\d|[a-z]{3}\/[a-z]{3}\/\d|[a-z]{2}\/\d{4}\?|[\w]{3}\/\d{6}$)/i;
+	var re = /[a-z]+:\/\/[\w.]+\/(([a-z]{1}\/\d)|([a-z]{3}\/[a-z]{3}\/\d)|([a-z]{2}\/\d{4}\?)|([\w]{3}\/\d{6}$)|([\d]{4}\/\d{2}\/\d{11}\.txt)|([\w]{2}\/\w{5}\/\d{5}\/\d{6})|(sh\/[\w]{2}\/\d{3}))/i;
 	
 	//这种格式均为广告网址
 	//https://knr.xxxxx.cn/j/140000		#[a-z]{1}\/\d{6}
-	//https://xx.xxx.xx/xxx/xxx/0000	#[a-z]{3}\/[a-z]+\/\d
+	//https://xx.xxx.xx/xxx/xxx/0000	#[a-z]{3}\/[a-z]{3}\/\d
 	//https://tg.xxx.com/sc/0000?n=xxxx #[a-z]{2}\/\d{4}\?
 	//https://xx.xxx.xyz/vh1/158051 	#[\w]{3}\/\d{6}$
+	//https://xx.xx.com/0000/00/23030926631.txt 	#[\d]{4}\/\d{2}\/\d{11}\.txt
+	//https://xxxxx.xxxxxx.com/v2/stats/12215/157527 	#[\w]{2}\/\w{5}\/\d{5}\/\d{6}
+	//https://xxx.xxxxxx.com/sh/to/853	#sh\/[\w]{2}\/\d{3}
 	
 	if(!re.test(url)){
 		return url;
