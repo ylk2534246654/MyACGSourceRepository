70838e5e68009f636dce45800747722e482f3ffa
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-03-16 16:33:20 +0800

    修复并不再兼容 1.4.3 以下版本

--- sources/多多漫画.js
+++ sources/多多漫画.js
@@ -8,15 +8,15 @@ function manifest() {
 		id: 1654843272,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20211219,
+		minMyACG: 20230315,
 		
 		//优先级1~100，数值越大越靠前
 		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
 		priority: 20,
 		
-		//是否失效，默认关闭
+		//是否启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		invalid: false,
+		isEnabledInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "多多漫画",
@@ -40,7 +40,7 @@ function manifest() {
 		},
 		
 		//更新时间
-		updateTime: "2022年12月10日",
+		updateTime: "2023年3月15日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
@@ -49,132 +49,166 @@ function manifest() {
 		contentType: 1,
 		
 		//自定义标签
-		tag: ["漫画"],
+		groupName: ["漫画"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "https://m.duoduomh.com",//备用：https://m.zuimh.com
+		baseUrl: baseUrl,
 	});
 }
-const header = '@ip->163.197.43.177';
+const baseUrl = "https://m.duoduomh.com";//备用：https://m.duoduomh.com  #https://m.zuimh.com
+const header = '';
 
 /**
  * 搜索
- * @params {string} key
- * @returns {[{title, summary, cover, url}]}
+ * @param {string} key
+ * @return {[{title, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = 'https://m.duoduomh.com/search/?keywords=' + encodeURI(key) + header;
-	const response = httpRequest(url);
-	
-	const list = jsoupArray(response,'#update_list > div > div').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
-			//标题
-			title : jsoup(data,'div.itemTxt > a').text(),
-			
-			//概览
-			summary : jsoup(data,'a.coll').text(),
-			
-			//封面
-			cover : jsoup(data,'div.itemImg > a > img').attr('src'),
-			
-			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'div.itemTxt > a').attr('href'))
+	var url = ToolUtils.urlJoin(baseUrl,'/search/?keywords=' + encodeURI(key) + header);
+	const response = HttpRequest(url);
+	var result = [];
+	if(response.code() == 200){
+		var document = response.document();
+		var elements = document.select("#update_list > div > div");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//标题
+				title: element.selectFirst('div.itemTxt > a').text(),
+				
+				//概览
+				summary: element.selectFirst('a.coll').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('div.itemImg > a > img').absUrl('src'),
+				
+				//网址
+				url: element.selectFirst('div.itemTxt > a').absUrl('href')
 			});
+		}
 	}
-	return JSON.stringify(array);
+	return JSON.stringify(result);
 }
 /**
  * 详情
- * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
+ * @return {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = httpRequest(url+ header);
-	return JSON.stringify({
-		//标题
-		title : jsoup(response,'#comicName').text(),
-		
-		//作者
-		author: jsoup(response,'div.sub_r > p:nth-child(1)').text(),
-		
-		//更新时间
-		date: jsoup(response,'span.date').text(),
-		
-		//概览
-		summary: jsoup(response,'p.txtDesc#full-des').text(),
+	const response = HttpRequest(url + header);
+	if(response.code() == 200){
+		var document = response.document();
+		return JSON.stringify({
+			//标题
+			title: document.selectFirst('#comicName,.title').text(),
+			
+			//作者
+			author: document.selectFirst('div.sub_r > p:nth-child(1),div.view-sub > div > dl:nth-child(3) > dd').text(),
+			
+			//日期
+			update: document.selectFirst('span.date,.clearfix > div.view-sub > div > dl:nth-child(5) > dd').text(),
+			
+			//概览
+			summary: document.selectFirst('p.txtDesc#full-des,.txtDesc').text(),
 
-		//封面
-		cover : jsoup(response,'#Cover > img').attr('src'),
-
-		//目录是否倒序
-		reverseOrder: false,
-		
-		//目录加载
-		catalog: catalog(response,url)
-	})
+			//封面网址
+			coverUrl: document.selectFirst('#Cover > img,#Cover > mip-img').absUrl('src'),
+			
+			//此处在 MyACG_V1.4.3 搞错了，原定使用 isEnabledChapterReverseOrder，
+			//目前暂时使用 isEnabledReverseOrder 进行代替，如果要兼容 MyACG_V1.4.3 建议把两个都加上
+			//是否启用将章节置为倒序
+			isEnabledReverseOrder: true,
+			//是否启用将章节置为倒序
+			isEnabledChapterReverseOrder: true,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
 }
 
 /**
  * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
+ * @return {[{name, chapters:{[{name, url}]}}]}
  */
-function catalog(response,url) {
-	//目录标签代码
-	const tabs = jsoupArray(response,'.caption').outerHtml();
+function tocs(document) {
+	const tagElements = document.select('.caption,.title1');
 	
-	//目录代码
-	const catalogs = jsoupArray(response,'div.chapter-warp').outerHtml();
+	//目录元素选择器
+	const tocElements= document.select('div.chapter-warp,.comic-chapters');
 	
 	//创建目录数组
-	var new_catalogs= [];
+	var newTocs = [];
 	
-	for (var i=0;i<catalogs.length;i++) {
-	    var catalog = catalogs[i];
-		
+	for (var i = 0;i < tocElements.size();i++) {
 		//创建章节数组
-		var newchapters= [];
+		var newChapters = [];
 		
-		//章节代码
-		var chapters = jsoupArray(catalog,'ul > li').outerHtml();
+		//章节元素选择器
+		var chapterElements = tocElements.get(i).select('ul > li');
 		
-		for (var ci=0;ci<chapters.length;ci++) {
-			var chapter = chapters[ci];
-			var name = jsoup(chapter,'a').text();
+		for (var i2 = 0;i2 < chapterElements.size();i2++) {
+			var chapterElement = chapterElements.get(i2);
+			var name = chapterElement.selectFirst('a').text();
 			if(name.indexOf("下拉式阅读") == -1){
-				newchapters.push({
+				newChapters.push({
 					//章节名称
 					name: name,
-					//章节链接
-					url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href').replace('.html','-${p}.html@zero->1@start->1'))
+					//章节网址
+					url: chapterElement.selectFirst('a').absUrl('href').replace('.html','-${p}.html@zero->1@start->1')
 				});
 			}
 		}
-		//添加目录
-		new_catalogs.push({
+		newTocs.push({
 			//目录名称
-			tag: jsoup(tabs[i],'.Title').text(),
+			name: tagElements.get(i).selectFirst('.Title,h3').text(),
 			//章节
-			chapter : newchapters
-			});
+			chapters: newChapters
+		});
 	}
-	return new_catalogs
+	return newTocs
 }
 
 /**
- * 内容
- * @params {string} url
+ * 内容（部分漫画搜索源通用规则）
+ * @version 2023/1/21
+ * 168,思思，39 , 360 , 147 , 动漫画 ，依依
  * @returns {string} content
  */
 function content(url) {
-	const response = httpRequest(url + header);
-	const src = jsoup(response,'div.image-content > img').attr('src');
-	if(src.indexOf('default') == -1){
-		return JSON.stringify(src);
+	const response = HttpRequest(url + header);
+	if(response.code() == 200){
+		var document = response.document();
+		var imgList = [];
+		var elements = document.select('\
+mip-link > img:not([style=display: none;]),\
+div.UnderPag > mip-img,\
+div.erPag > mip-img,\
+mip-link > mip-img:not([style=display: none;]),\
+div:not([style]) > mip-link > mip-img:not([style],[width]),\
+mip-link > mip-img,\
+#image,\
+#scroll-image > div > [src],\
+#scroll-image > div > [data-src]');
+		if(elements != null){
+			for (var i = 0;i<elements.size();i++) {
+				var element = elements.get(i);
+				var src = element.absUrl('data-src');
+				if(String(src).length <= 0){
+					src = element.absUrl('src');
+				}
+				imgList.push(src);
+			}
+		}
+
+		var newImgList = [];
+		for(var i = 0;i < imgList.length;i++){
+			var re = /default|cover|\.gif|\/manhua\//i;
+			if(!re.test(imgList[i])){
+				newImgList.push(imgList[i]);
+			}
+		}
+		return JSON.stringify(newImgList);
 	}
 	return null;
 }
