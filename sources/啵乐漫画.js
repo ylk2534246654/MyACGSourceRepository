e18e99055a78d8261aca727f020f2739832bed0d
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-03-16 13:11:50 +0800

    优化并不再兼容 1.4.3 以下版本

--- sources/啵乐漫画.js
+++ sources/啵乐漫画.js
@@ -1,22 +1,22 @@
 function manifest() {
 	return JSON.stringify({
 		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
+		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
 		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1670670509,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20211219,
+		minMyACG: 20230315,
 		
 		//优先级1~100，数值越大越靠前
 		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
 		priority: 10,
 		
-		//是否失效，默认关闭
+		//是否启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		invalid: false,
+		isEnabledInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "啵乐漫画",
@@ -28,7 +28,7 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 2,
+		version: 3,
 
 		//搜索源自动同步更新网址
 		syncList: {
@@ -40,7 +40,7 @@ function manifest() {
 		},
 		
 		//更新时间
-		updateTime: "2022年12月10日",
+		updateTime: "2023年3月15日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
@@ -49,13 +49,13 @@ function manifest() {
 		contentType: 1,
 		
 		//自定义标签
-		tag: ["漫画"],
+		groupName: ["漫画"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "http://www.bolebl.com",//备用：http://www.404ms.top
+		baseUrl: baseUrl,
 	});
 }
-
+const baseUrl = "http://www.bolebl.com";//备用：http://www.404ms.top
 const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
 
 /**
@@ -64,100 +64,100 @@ const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) Ap
  * @returns {[{title, summary, cover, url}]}
  */
 function search(key) {
-	var url = 'http://www.bolebl.com/search?keyword=' + encodeURI(key) + header;
-	const response = httpRequest(url);
-	
-	const list = jsoupArray(response,'.acgn-item').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
-			//标题
-			title : jsoup(data,'.acgn-title').text(),
-			
-			//概览
-			summary : jsoup(data,'.acgn-desc').text(),
-			
-			//封面
-			cover : ToolUtil.urlJoin(url,ToolUtil.substring(jsoup(data,'img').attr('style'),'url(\'','\'')),
-			
-			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'.acgn-thumbnail').attr('href'))
-			});
+	var url = ToolUtils.urlJoin(baseUrl,'/search?keyword=' + encodeURI(key) + header);
+	const response = HttpRequest(url);
+	var result = [];
+	if(response.code() == 200){
+		var document = response.document();
+        var elements = document.select(".acgn-item");
+        for (var i = 0;i < elements.size();i++) {
+            var element = elements.get(i);
+            result.push({
+                //标题
+                title: element.selectFirst('.acgn-title').text(),
+                
+                //概览
+                summary: element.selectFirst('.acgn-desc').text(),
+                
+                //封面网址
+				coverUrl : ToolUtils.urlJoin(url,ToolUtils.substring(element.selectFirst('img').attr('style'),'url(\'','\'')),
+                
+                //网址
+                url: element.selectFirst('.acgn-thumbnail').absUrl('href')
+            });
+        }
 	}
-	return JSON.stringify(array);
+	return JSON.stringify(result);
 }
 /**
  * 详情
- * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
+ * @returns {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = httpRequest(url+ header);
-	return JSON.stringify({
-		//标题
-		title : jsoup(response,'div.content > h1').text(),
-		
-		//作者
-		author: jsoup(response,'div.content > p > span:nth-child(4)').text(),
-		
-		//日期
-		date : jsoup(response,'.update').text(),
-		
-		//概览
-		summary: jsoup(response,'.desc-content').text(),
-
-		//封面
-		cover : ToolUtil.urlJoin(url,ToolUtil.substring(jsoup(response,'div.detail-cover > img').attr('style'),'url(\'','\'')),
-		
-		//目录是否倒序
-		reverseOrder: false,
-		
-		//目录加载
-		catalog: catalog(response,url)
-	})
+	const response = HttpRequest(url + header);
+	if(response.code() == 200){
+		var document = response.document();
+		return JSON.stringify({
+			//标题
+			title: document.selectFirst('div.content > h1').text(),
+			
+			//作者
+			author: document.selectFirst('div.content > p > span:nth-child(4)').text(),
+			
+			//更新时间
+			update: document.selectFirst('.update').text(),
+			
+			//概览
+			summary: document.selectFirst('.desc-content').text(),
+	
+			//封面网址
+			cover : ToolUtils.urlJoin(url,ToolUtils.substring(document.selectFirst('div.detail-cover > img').attr('style'),'url(\'','\'')),
+			
+			//是否启用将章节置为倒序
+			isEnabledChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
 }
 
 /**
  * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
+ * @returns {[{name, chapters:{[{name, url}]}}]}
  */
-function catalog(response,url) {
+function tocs(document) {
 	//创建目录数组
-	var new_catalogs= [];
-		
+	var newTocs = [];
+	
 	//创建章节数组
-	var newchapters= [];
+	var newChapters = [];
 		
-	//章节代码
-	var chapters = jsoupArray(response,'.chapter-list > li').outerHtml();
+	//章节元素选择器
+	var chapterElements = document.select('.chapter-list > li');
+	
+	for (var i2 = 0;i2 < chapterElements.size();i2++) {
+		var chapterElement = chapterElements.get(i2);
 		
-	for (var ci=0;ci<chapters.length;ci++) {
-		var chapter = chapters[ci];
-			
-		newchapters.push({
+		newChapters.push({
 			//章节名称
-			name: jsoup(chapter,'a').text(),
+			name: chapterElement.selectFirst('a').text(),
 			//章节网址
-			url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
+			url: chapterElement.selectFirst('a').absUrl('href')
 		});
 	}
-	//添加目录
-	new_catalogs.push({
+	newTocs.push({
 		//目录名称
-		tag: '目录',
+		name: '目录',
 		//章节
-		chapter : newchapters
+		chapters: newChapters
 	});
-	return new_catalogs;
+	return newTocs
 }
 
 /**
- * 内容（部分漫画搜索源通用规则）
  * @version 2022/12/8
- * 168,思思，39 , 360 , 147 , 动漫画 ，依依
  * @params {string} url
  * @returns {string} content
  */
