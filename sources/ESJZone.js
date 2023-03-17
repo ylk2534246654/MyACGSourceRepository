f07bb341c2c5434d2342b4b638e360158186fa97
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-03-17 22:30:35 +0800

    修复并不再兼容 1.4.3.2 以下版本

--- sources/ESJZone.js
+++ sources/ESJZone.js
@@ -1,22 +1,22 @@
 function manifest() {
 	return JSON.stringify({
 		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
+		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
 		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1652592780,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220101,
+		minMyACG: 20230317,
 
 		//优先级1~100，数值越大越靠前
 		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
 		priority: 30,
 		
-		//是否失效，默认关闭
+		//是否启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		invalid: false,
+		isEnabledInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "ESJ Zone",
@@ -41,7 +41,7 @@ function manifest() {
 		},
 		
 		//更新时间
-		updateTime: "2022年3月29日",
+		updateTime: "2023年3月17日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 4,
@@ -53,10 +53,14 @@ function manifest() {
 		tag: ["小说","轻小说"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "https://www.esjzone.net",//如果失效建议贴吧搜索最新网址
+		baseUrl: baseUrl,//如果失效建议贴吧搜索最新网址
 	});
 }
-
+const baseUrl = "https://www.esjzone.me";
+/**
+ * 网站记录
+ * www.esjzone.net
+ */
 const header = '';
 /**
  * 搜索
@@ -64,114 +68,108 @@ const header = '';
  * @returns {[{title, summary, cover, url}]}
  */
 function search(key) {
-	var url = 'https://www.esjzone.net/tags/'+ encodeURI(key) + header;
-	const response = httpRequest(url);
-	
-	const list = jsoupArray(response,'div > div.row > div').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
-			//标题
-			title : jsoup(data,'h5.card-title > a').text(),
-			
-			//概览
-			summary : jsoup(data,'div.card-ep').text(),
-			
-			//封面
-			cover : jsoup(data,'div.main-img > div').attr('data-src'),
-			
-			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'h5.card-title > a').attr('href'))
-			});
-	}
-	return JSON.stringify(array);
-}
-/**
- * 详情
- * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
- */
-function detail(url) {
-	const response = httpRequest(url+ header);
-	return JSON.stringify({
-		//标题
-		title : jsoup(response,'div.book-detail > h2.text-normal').text(),
-		
-		//作者
-		author: jsoup(response,'ul.book-detail > li:nth-child(2) > a').text(),
-		
-		//日期
-		date : jsoup(response,'ul.book-detail > li:nth-child(3) > :matchText').text(),
-		
-		//概览
-		summary: jsoup(response,'div.description').text(),
-
-		//封面
-		cover : jsoup(response,'div.product-gallery > a > img').attr('src'),
-		
-		//目录是否倒序
-		reverseOrder: false,
-		
-		//目录网址/非外链无需使用
-		catalog: catalog(response,url)
-	})
-}
-/**
- * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
- */
-function catalog(response,url) {
-	//创建目录数组
-	var new_catalogs= [];
-		
-	//创建章节数组
-	var newchapters= [];
-		
-	//章节代码
-	var chapters = jsoupArray(response,'#chapterList > p,#chapterList > a').outerHtml();
-		
-	var group;//分组记录
-	for (var ci=0;ci<chapters.length;ci++) {
-		var chapter = chapters[ci];
-		
-		if(!(chapter.indexOf('href')!=-1)){
-			group = jsoup(chapter,':matchText').text();
-		}else{
-			var name;
-			if(group != null){
-				name = group + ' ' + jsoup(chapter,'a').text()
-			}else{
-				name = jsoup(chapter,'a').text();
-			}
-			newchapters.push({
-				//章节名称
-				name: name,
-				//章节网址
-				url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
+	var url = ToolUtils.urlJoin(baseUrl,'/tags/' + encodeURI(key) + header);
+	const response = HttpRequest(url + header);
+	var result= [];
+	if(response.code() == 200){
+		var document = response.document();
+		var elements = document.select("div > div.row > div");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//标题
+				title: element.selectFirst('h5.card-title > a').text(),
+				
+				//概览
+				summary: element.selectFirst('div.card-ep').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('div.main-img > div').absUrl('data-src'),
+				
+				//网址
+				url: element.selectFirst('h5.card-title > a').absUrl('href')
 			});
 		}
 	}
-	//添加目录
-	new_catalogs.push({
+	return JSON.stringify(result);
+}
+/**
+ * 详情
+ * @return {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ */
+function detail(url) {
+	const response = HttpRequest(url + header);
+	if(response.code() == 200){
+		var document = response.document();
+		return JSON.stringify({
+			//标题
+			title: document.selectFirst('div.book-detail > h2.text-normal').text(),
+			
+			//作者
+			author: document.selectFirst('ul.book-detail > li:nth-child(2) > a').text(),
+			
+			//日期
+			update: document.selectFirst('ul.book-detail > li:nth-child(3) > :matchText').text(),
+			
+			//概览
+			summary: document.selectFirst('div.description').text(),
+
+			//封面网址
+			coverUrl: document.selectFirst('div.product-gallery > a > img').absUrl('src'),
+			
+			//是否启用将章节置为倒序
+			isEnabledChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
+}
+/**
+ * 目录
+ * @return {[{name, chapters:{[{name, url}]}}]}
+ */
+function tocs(document) {
+	//创建章节数组
+	var newChapters= [];
+		
+	//章节元素选择器
+	var chapterElements = document.select('#chapterList > p,#chapterList > a');
+		
+	var group = '';//分组记录
+	for (var ci = 0;ci < chapterElements.size(); ci++) {
+		var chapterElement = chapterElements.get(ci);
+		
+		var aElement = chapterElement.selectFirst('a').text();
+		if(String(aElement).length > 0){
+			newChapters.push({
+				//章节名称
+				name: group + chapterElement.selectFirst('a').text(),
+				//章节网址
+				url: chapterElement.selectFirst('a').absUrl('href')
+				//url: chapterElement.selectFirst('a').absUrl('href').replace('.html','_${p}.html') + '@zero->1@start->1'
+			});
+		}else{
+			group = chapterElement.selectFirst(':matchText').text() + ' ';
+		}
+	}
+	return [{
 		//目录名称
 		tag: '目录',
 		//章节
-		chapter : newchapters
-	});
-	return new_catalogs;
+		chapter : newChapters
+	}];
 }
 
 /**
  * 内容
- * @params {string} url
- * @returns {string} content
+ * @return {string} content
  */
 function content(url) {
-	const response = httpRequest(url + header);
-	const content = jsoup(response,'.forum-content,#content').outerHtml();
-	return content;
+	const response = HttpRequest(url + header);
+	if(response.code() == 200){
+		return response.document().selectFirst('.forum-content,#content').outerHtml();
+	}
+	return null;
 }
-
