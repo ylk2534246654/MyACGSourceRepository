830a00f7ad61c8395066d39d164cc2ca3717ba37
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-08-18 05:27:06 +0800

    修复并不再兼容 1.4.5.1 以下版本

--- sources/铅笔小说.js
+++ sources/铅笔小说.js
@@ -1,34 +1,30 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1674623577,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20230207,
+		minMyACG: 20230815,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 20,
 		
-		//是否失效，默认关闭
+		//是否启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isInvalid: false,
+		isEnabledInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "铅笔小说",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 1,
+		version: 2,
 
 		//搜索源自动同步更新网址
 		syncList: {
@@ -40,122 +36,213 @@ function manifest() {
 		},
 		
 		//更新时间
-		updateTime: "2023年2月9日",
+		updateTime: "2023年8月17日",
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 4,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 1,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 1,
 		
-		//自定义标签
+		//分组
 		group: ["轻小说","小说"],
 		
 		//@NonNull 详情页的基本网址
 		baseUrl: baseUrl,
 		
-		//是否启用登录授权
-		auth: true,
+		//发现
+		findList: {
+			category: {
+				"label": {
+					"网络小说": "0",
+					"言情女生": "1",
+					"玄幻奇幻": "2",
+					"都市青春": "3",
+					"武侠仙侠": "4",
+					"耽美同人": "5",
+					"科幻灵异": "6",
+					"轻小说の": "7",
+					"历史军事": "8"
+				},
+				"order": {
+					"全部": "quanben",
+					"总点击": "allvisit",
+					"月点击": "monthvisit",
+					"周点击": "weekvisit",
+					"日点击": "dayvisit",
+					"总推荐": "allvote",
+					"月推荐": "monthvote",
+					"周推荐": "weekvote",
+					"日推荐": "dayvote",
+					"总收藏": "goodnum",
+					"字数": "size",
+					"入站时间": "postdate",
+					"更新时间": "lastupdate"
+				},
+				"size": {
+					"全部": "0",
+					"30万以下": "1",
+					"30万-50万": "2",
+					"50万-100万": "3",
+					"100万-200万": "4",
+					"200万以上": "5",
+				},
+				"status": {
+					"全部": "0",
+					"连载": "1",
+					"完本": "2",
+				}
+			},
+			"小说": ["label","order","size","status"]
+		},
+
+		//是否启用登录
+		isEnabledLogin: true,
 		
-		//登录授权网址
-		authUrl:"https://www.23qb.net/login.php",
+		//登录网址
+		loginUrl: "https://www.23qb.net/login.php",
 		
-		//需要授权的功能（search，detail，content，find）
-		authRequire: ["search"],
+		//需要登录的功能（search，detail，content，find）
+		requiresLoginList: ["search"],
+
+		//全局 HTTP 请求头列表
+		httpRequestHeaderList: {
+			"user-agent-system": "Windows NT 10.0; Win64; x64"
+		}
 	});
 }
+
 /*
- * 拦截并验证手动授权数据
- * @params {string} html	网页源码
- * @params {string} url		网址
- * @returns 是否授权
+ * 是否完成登录
+ * @param {string} url		网址
+ * @param {string} responseHtml	响应源码
+ * @return {boolean}  登录结果
  */
-function authCallback(html,url) {
-	if(html.length > 1 && html.indexOf('登录成功') != -1){
+function isUserLoggedIn(url, responseHtml) {
+	if(responseHtml != null && responseHtml.length > 1 && responseHtml.indexOf('登录成功') != -1){
 		return true;
 	}
 	return false;
 }
+
 /*
- * 自动验证授权结果
- * @returns 是否授权
+ * 验证完成登录
+ * @return {boolean} 登录结果
  */
-function authVerify() {
-	const response = httpRequest("https://www.23qb.net/saerch.php" + header);
-	if(response.indexOf('个人中心') != -1){
-		return true;
+function verifyUserLoggedIn() {
+	const response = JavaUtils.httpRequest(JavaUtils.urlJoin(baseUrl, "https://www.23qb.net/saerch.php"));
+	if(response.code() == 200){
+		if(response.body().string().indexOf('个人中心') != -1){
+			return true;
+		}
 	}
 	return false;
 }
 
 const baseUrl = "https://www.23qb.net";
-const header = '@header->user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.7';
+
 /**
  * 搜索
- * @params {string} key
- * @returns {[{title, summary, coverUrl, url}]}
+ * @param {string} key
+ * @return {[{name, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = ToolUtil.urlJoin(baseUrl,'/saerch.php@post->searchkey=' + ToolUtil.encodeURI(key,'gbk') + '&searchtype=all' + header);
-	const response = httpRequest(url);
-	
-	var document = org.jsoup.Jsoup.parse(response,url);
+	var url = JavaUtils.urlJoin(baseUrl, `/saerch.php@post->searchkey=${JavaUtils.encodeURI(key,'gbk')}&searchtype=all`);
 	var result = [];
-    var elements = document.select("#sitebox > dl");
-	for (var i = 0;i < elements.size();i++) {
-	    var element = elements.get(i);
-		result.push({
-			//标题
-			title: element.selectFirst('dd > h3 > a').text(),
-			
-			//概览
-			summary: element.selectFirst('#nr > dd.book_des').text(),
-			
-			//封面网址
-			coverUrl: element.selectFirst('dt > a > img').absUrl('_src'),
-			
-			//网址
-			url: element.selectFirst('dd > h3 > a').absUrl('href')
-		});
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		var elements = document.select("#sitebox > dl");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('dd > h3 > a').text(),
+				
+				//概览
+				summary: element.selectFirst('#nr > dd.book_des').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('dt > a > img').absUrl('_src'),
+				
+				//网址
+				url: element.selectFirst('dd > h3 > a').absUrl('href')
+			});
+		}
 	}
 	return JSON.stringify(result);
 }
+
+/**
+ * 发现
+ * @return {[{name, summary, coverUrl, url}]}
+ */
+function find(label, order, size, status) {
+	var url = JavaUtils.urlJoin(baseUrl, `/book/${label}-${order}-0-${size}-0-0-${status}-0-1.html`);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		var elements = document.select("#sitebox > dl");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('dd > h3 > a').text(),
+				
+				//概览
+				summary: element.selectFirst('#nr > dd.book_des').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('dt > a > img').absUrl('_src'),
+				
+				//网址
+				url: element.selectFirst('dd > h3 > a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
 /**
  * 详情
- * @returns {[{title, author, date, summary, coverUrl, isReverseOrder, catalogs:{[{name, chapters:{[{name, url}]}}]}}]}
+ * @return {[{name, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = httpRequest(url + header);
-	var document = org.jsoup.Jsoup.parse(response,url);
-	return JSON.stringify({
-		//标题
-		title : document.selectFirst('div.d_title').text(),
-		
-		//作者
-		author: document.selectFirst('#count > ul > li:nth-child(1) > a').text(),
-		
-		//日期
-		date: document.selectFirst('#uptime > span').text(),
-		
-		//概览
-		summary: document.selectFirst('#bookintro').text(),
-
-		//封面网址
-		coverUrl: document.selectFirst('#bookimg > img').absUrl('src'),
-		
-		//目录是否倒序
-		isReverseOrder: false,
-		
-		//目录网址/非外链无需使用
-		catalogs: catalogs(document)
-	});
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		return JSON.stringify({
+			//标题
+			name: document.selectFirst('div.d_title').text(),
+			
+			//作者
+			author: document.selectFirst('#count > ul > li:nth-child(1) > a').text(),
+			
+			//更新时间
+			update: document.selectFirst('#uptime > span').text(),
+			
+			//概览
+			summary: document.selectFirst('#bookintro').text(),
+	
+			//封面网址
+			coverUrl: document.selectFirst('#bookimg > img').absUrl('src'),
+			
+			//是否启用将章节置为倒序
+			isEnabledChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
 }
 
 /**
  * 目录
  * @returns {[{name, chapters:{[{name, url}]}}]}
  */
-function catalogs(document) {
+function tocs(document) {
 	//创建章节数组
 	var newChapters= [];
 	
@@ -168,7 +255,7 @@ function catalogs(document) {
             //章节名称
             name: chapterElement.text(),
             //章节网址
-            url: chapterElement.selectFirst('a').absUrl('href').replace('.html','_${p}.html') + '@zero->1@start->1'
+            url: chapterElement.selectFirst('a').absUrl('href').replace('.html','_{1}.html')
         });
 	}
 	return [{
