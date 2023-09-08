936efa36a27a87462853423db236f3e44193b24d
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-09-08 18:06:31 +0800

    Add files via upload

---
+++ sources/AGE动漫.js
@@ -0,0 +1,287 @@
+function manifest() {
+	return JSON.stringify({
+		//@NonNull 搜索源 ID 标识，设置后不建议更改
+		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
+		id: 1648714123,
+		
+		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
+		minMyACG: 20230815,
+
+		//优先级 1~100，数值越大越靠前
+		priority: 80,
+		
+		//是否启用失效#默认关闭
+		//true: 无法安装，并且已安装的变灰，用于解决失效源
+		isEnabledInvalid: false,
+		
+		//@NonNull 搜索源名称
+		name: "AGE动漫",
+
+		//搜索源作者
+		author: "雨夏",
+
+		//电子邮箱
+		email: "2534246654@qq.com",
+
+		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
+		version: 5,
+
+		//搜索源自动同步更新网址
+		syncList: {
+			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/AGE动漫.js",
+			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/AGE动漫.js",
+			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/AGE动漫.js",
+			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/AGE动漫.js",
+		},
+		
+		//更新时间
+		updateTime: "2023年8月15日",
+		
+		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
+		type: 3,
+		
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 2,
+		
+		//分组
+		group: ["动漫"],
+		
+		//@NonNull 详情页的基本网址
+		baseUrl: baseUrl,//备用 http://age.tv
+		
+		//发现
+		findList: {
+			category: {
+				"region": ["全部","日本","欧美"],
+				"genre": ["全部","TV","剧场版","OVA"],
+				"status": ["全部","连载中","已完结","未播放"],
+				"label": ["全部","搞笑","运动","励志","武侠","特摄","热血","战斗","竞技","校园","青春","爱情","冒险","后宫","百合","治愈","萝莉","魔法","悬疑","推理","奇幻","神魔","恐怖","血腥","机战","战争","犯罪","社会","职场","剧情","伪娘","耽美","歌舞","肉番","美少女","吸血鬼","泡面番","欢乐向"],
+				"year": ["全部","2023","2022","2021","2020","2019","2018","2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","更早"],
+				"resources": ["全部","BDRIP","AGE-RIP"],
+				"order": {
+					"时间排序": "time",
+					"点击量": "点击量",
+				},
+			},
+			"动漫": {
+				default: ["genre","year","label","resources","order","region","status"],
+				"每日推荐": {
+					"param": "/recommend",
+					"function": "find2"
+				}
+			}
+		},
+
+		//网络限流 - 如果{regexUrl}匹配网址，则限制其{period}毫秒内仅允许{maxRequests}个请求
+		networkRateLimitList: [
+			{
+				regexUrl: "/search",//表示需要限流的 Url，使用正则表达式格式（不允许为空）
+				maxRequests: 0,//在指定的时间内允许的请求数量（必须 >= 0 才会生效）
+				period: 3000,//时间周期，毫秒（必须 > 0 才会生效）
+			}
+		],
+
+		//全局 HTTP 请求头列表
+		httpRequestHeaderList: {
+			//"user-agent-system": "Windows NT 10.0; Win64; x64"
+		}
+	});
+}
+
+const baseUrl = getBaseUrl();
+
+function getBaseUrl() {
+	var preference = JavaUtils.getPreference();
+	var baseUrlTime = preference.getLong("baseUrlTime");
+	var oneDay = 1000*60*60*24;
+	var time = new Date().getTime();
+	if(baseUrlTime < time - oneDay){//超过一天
+		const response = JavaUtils.httpRequest("https://rentry.org/agefans");
+		var edit = preference.edit();
+		if(response.code() == 200){
+			var _baseUrl = JavaUtils.substring(response.body().string(),"新版：","\n");
+			if(JavaUtils.isNetworkUrl(_baseUrl)){
+				edit.putString("baseUrl", _baseUrl);//更新基础网址
+			}
+		}
+		edit.putLong("baseUrlTime", time).apply();//更新时间
+	}
+	return preference.getString("baseUrl", "https://www.agemys.org");
+}
+
+/**
+ * 搜索
+ * @param {string} key
+ * @return {[{name, summary, coverUrl, url}]}
+ */
+function search(key) {
+	var url = JavaUtils.urlJoin(baseUrl, '/search?&query=' + encodeURI(key));
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		var elements = document.select("#cata_video_list > div > div");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('.card-title').text(),
+				
+				//概览
+				summary: element.selectFirst('div.video_cover > div > span').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('div.video_cover > div > a > img').absUrl('data-original'),
+				
+				//网址
+				url: element.selectFirst('.card-title > a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
+/**
+ * 发现
+ * @return {[{name, summary, coverUrl, url}]}
+ */
+function find(genre, year, label, resources, order, region, status) {
+	if(genre == "全部")genre = "all";
+	if(year == "全部")year = "all";
+	if(year == "更早")year = "2000以前";
+	if(label == "全部")label = "all";
+	if(resources == "全部")resources = "all";
+	if(region == "全部")region = "all";
+	if(status == "全部")status = "all";
+	
+	var url = JavaUtils.urlJoin(baseUrl, `/catalog/${genre}-${year}-all-${label}-${resources}-${order}-1-${region}-all-${status}`);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		const elements = document.select("#cata_video_list > div > div");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('.card-title').text(),
+				
+				//概览
+				summary: element.selectFirst('div.video_cover > div > span').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('div.video_cover > div > a > img').absUrl('data-original'),
+				
+				//网址
+				url: element.selectFirst('.card-title > a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+/**
+ * 发现2
+ * @return {[{name, summary, coverUrl, url}]}
+ */
+function find2(url) {
+	var url = JavaUtils.urlJoin(baseUrl, url);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		var elements = document.select("div.video_item");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('.video_item-title > a').text(),
+				
+				//概览
+				summary: element.selectFirst('.video_item--image > span').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('.video_item--image > img').absUrl('data-original'),
+				
+				//网址
+				url: element.selectFirst('.video_item-title > a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
+/**
+ * 详情
+ * @return {[{name, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ */
+function detail(url) {
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		return JSON.stringify({
+			//标题
+			name: document.selectFirst('.video_detail_title').text(),
+			
+			//作者
+			author: document.selectFirst('li:nth-child(5) > span.detail_imform_value').text(),
+			
+			//更新时间
+			update: document.selectFirst('li:nth-child(7) > span.detail_imform_value').text(),
+			
+			//概览
+			summary: document.selectFirst('div.video_detail_desc').text(),
+	
+			//封面网址
+			coverUrl: document.selectFirst('div.video_detail_cover > img').absUrl('data-original'),
+			
+			//是否启用将章节置为倒序
+			isEnabledChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
+}
+
+/**
+ * 目录
+ * @returns {[{name, chapters:{[{name, url}]}}]}
+ */
+function tocs(document) {
+	//目录标签元素选择器
+	const tagElements = document.select('.nav-item');
+	
+	//目录元素选择器
+	const tocElements = document.select('.tab-pane');
+	
+	//创建目录数组
+	var newTocs = [];
+	
+	for (var i = 0;i < tocElements.size();i++) {
+		//创建章节数组
+		var newChapters = [];
+		
+		//章节元素选择器
+		var chapterElements = tocElements.get(i).select('ul > li');
+		
+		for (var i2 = 0;i2 < chapterElements.size();i2++) {
+			var chapterElement = chapterElements.get(i2);
+			
+			newChapters.push({
+				//章节名称
+				name: chapterElement.selectFirst('a').text(),
+				//章节网址
+				url: chapterElement.selectFirst('a').absUrl('href')
+			});
+		}
+		newTocs.push({
+			//目录名称
+			name: tagElements.get(i).selectFirst('li').text(),
+			//章节
+			chapters : newChapters
+		});
+	}
+	return newTocs;
+}
+
---
+++ sources/樱花动漫P.js
@@ -0,0 +1,275 @@
+function manifest() {
+	return JSON.stringify({
+		//@NonNull 搜索源 ID 标识，设置后不建议更改
+		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
+		id: 1652947579,
+		
+		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
+		minMyACG: 20230804,
+
+		//优先级 1~100，数值越大越靠前
+		priority: 70,
+		
+		//是否启用失效#默认关闭
+		//true: 无法安装，并且已安装的变灰，用于解决失效源
+		isEnabledInvalid: false,
+		
+		//@NonNull 搜索源名称
+		name: "樱花动漫P",
+
+		//搜索源作者
+		author: "雨夏",
+
+		//电子邮箱
+		email: "2534246654@qq.com",
+
+		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
+		version: 10,
+
+		//搜索源自动同步更新链接
+		syncList: {
+			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/樱花动漫P.js",
+			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/樱花动漫P.js",
+			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/樱花动漫P.js",
+			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/樱花动漫P.js",
+			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/樱花动漫P.js",
+		},
+		
+		//更新时间
+		updateTime: "2023年8月4日",
+		
+		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
+		type: 3,
+		
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 2,
+		
+		//自定义标签
+		group: ["动漫"],
+		
+		//@NonNull 详情页的基本网址
+		baseUrl: baseUrl,
+		
+		//发现
+		findList: {
+			category: {
+				"region": ["全部","日本","欧美","其他"],
+				"genre": ["全部","TV","剧场版","WEB","OVA","其他"],
+				"status": ["全部","连载中","已完结","未播放"],
+				"label": ["全部","搞笑","运动","励志","武侠","特摄","热血","战斗","竞技","校园","青春","爱情","冒险","后宫","百合","治愈","萝莉","魔法","悬疑","推理","奇幻","神魔","恐怖","血腥","机战","战争","犯罪","社会","职场","剧情","伪娘","耽美","歌舞","肉番","美少女","吸血鬼","泡面番","欢乐向","其他"],
+				"year": ["全部","2023","2022","2021","2020","2019","2018","2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","更早"],
+				"order": ["时间排序","名称","点击量"],
+			},
+			"动漫": ["region", "genre", "year", "status", "label", "order"]
+		},
+
+		//全局 HTTP 请求头列表
+		httpRequestHeaderList: {
+			"user-agent": "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36 Edg/112.0.0.0",
+			"user-agent-platform": "Win32",
+		}
+	});
+}
+const baseUrl = "https://www.yhdmz.org";
+/**
+ * 已关闭网址
+ * yhdmp.live
+ * yhdmp.net
+ * yhdmp.me
+ * 
+ * 和风车动漫P是同源同模板不同名的网站
+ */
+
+/**
+ * 搜索
+ * @param {string} key
+ * @return {[{name, summary, coverUrl, url}]}
+ */
+function search(key) {
+	var url = JavaUtils.urlJoin(baseUrl, '/s_all?ex=1&kw=' + encodeURI(key));
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		var elements = document.select("div[class] > ul:not([class]) > li:not([class])");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('h2').text(),
+				
+				//概览
+				summary: element.selectFirst('span').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('img[src]').absUrl('src'),
+				
+				//网址
+				url: element.selectFirst('h2 > a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
+
+/**
+ * 发现
+ * @return {[{name, summary, coverUrl, url}]}
+ */
+function find(region, genre, year, status, label, order) {
+	if(region == "全部")region = "";
+	if(genre == "全部")genre = "";
+	if(status == "全部")status = "";
+	if(label == "全部")label = "";
+	if(year == "全部")year = "";
+
+	var url = JavaUtils.urlJoin(baseUrl, `/list/?region=${encodeURI(region)}&genre=${encodeURI(genre)}&year=${encodeURI(year)}&status=${encodeURI(status)}&label=${encodeURI(label)}&order=${encodeURI(order)}`);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		const elements = document.select("div[class] > ul:not([class]) > li:not([class])");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('h2').text(),
+				
+				//概览
+				summary: element.selectFirst('span').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('img[src]').absUrl('src'),
+				
+				//网址
+				url: element.selectFirst('h2 > a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
+/**
+ * 详情
+ * @return {[{name, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ */
+function detail(url) {
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		return JSON.stringify({
+			//标题
+			name: document.selectFirst('div:nth-child(2) > div > h1').text(),
+			
+			//作者
+			author: document.selectFirst('div.info-sub > p:nth-child(1)').text(),
+			
+			//更新时间
+			//update: document.selectFirst('').text(),
+			
+			//概览
+			summary: document.selectFirst('div.info').text(),
+	
+			//封面网址
+			coverUrl: document.selectFirst('div.show > img').absUrl('src'),
+			
+			//是否启用将章节置为倒序
+			isEnabledChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
+}
+
+/**
+ * 目录
+ * @return {[{name, chapters:{[{name, url}]}}]}
+ */
+function tocs(document) {
+	const tagElements = document.select('#menu0 > li');
+	
+	//目录元素选择器
+	const tocElements = document.select('#main0 > div.movurl');
+	
+	//创建目录数组
+	var newTocs = [];
+	
+	for (var i = 0;i < tocElements.size();i++) {
+		//创建章节数组
+		var newChapters = [];
+		
+		//章节元素选择器
+		var chapterElements = tocElements.get(i).select('ul > li');
+		
+		for (var i2 = 0;i2 < chapterElements.size();i2++) {
+			var chapterElement = chapterElements.get(i2);
+			
+			newChapters.push({
+				//章节名称
+				name: chapterElement.selectFirst('a').text(),
+				//章节网址
+				url: chapterElement.selectFirst('a').absUrl('href')
+			});
+		}
+		newTocs.push({
+			//目录名称
+			name: tagElements.get(i).selectFirst('li').text(),
+			//章节
+			chapters: newChapters
+		});
+	}
+	return newTocs
+}
+
+/**
+ * 内容（部分搜索源通用过滤规则）
+ * @version 2023/4/27
+ * 布米米、嘻嘻动漫、12wo动漫、路漫漫、风车动漫P、樱花动漫P、COCO漫画、Nike
+ * @return {string} content
+ */
+function content(url) {
+	var re = new RegExp(
+		//https://
+		'[a-z]+://[\\w.:]+/(' +
+
+		//https://knr.xxxxx.cn/j/140000		#[a-z]{1}\/\d{6}
+		'([a-z]{1}/\\d)' +
+
+		//https://xx.xxx.xx/xxx/xxx/0000	#[a-z]{3}\/[a-z]{3}\/\d
+		'|([a-z]{3}/[a-z]{3}/\\d)' +
+
+		//https://tg.xxx.com/sc/0000?n=xxxx #[a-z]{2}\/\d{4}\?
+		'|([a-z]{2}/\\d{4}\\?)' +
+
+		//https://xx.xxx.xyz/vh1/158051 	#[\w]{3}\/\d{6}$
+		'|([\\w]{3}/\\d{6}$)' +
+
+		//https://xx.xx.com/0000/00/23030926631.txt 	#[\d]{4}\/\d{2}\/\d{11}\.txt
+		'|([\\d]{4}/\\d{2}/\\d{11}\\.txt)' +
+
+		//https://xxxxx.xxxxxx.com/v2/stats/12215/157527 	#[\w]{2}\/\w{5}\/\d{5}\/\d{6}
+		'|([\\w]{2}/\\w{5}/\\d{5}/\\d{6})' +
+
+		//https://xxx.xxxxxx.com/sh/to/853	#sh\/[\w]{2}\/\d{3}
+		'|(sh/[\\w]{2}/\\d{3})' +
+
+		//https://xxx.rmb.xxxxxxxx.com/xxx/e3c5da206d50f116fc3a8f47502de66d.gif #[\w]{3}\/[\w]{32}\.
+		'|([\\w]{3}/[\\w]{32}\\.)' +
+
+		//https://xxxx.xxxx.xx:00000/mnrt/kmrr1.woff
+		//https://xxxx.xxxx.xx:00000/kmopef/3.woff # [\w/]+[/km][\w/]+\.woff
+		'|([\\w/]+[/km][\\w/]+\\.woff)' +
+
+		')'+
+		''
+		,
+		'i'
+	);
+	if(!re.test(url)){
+		return url;
+	}
+	return null;
+}
