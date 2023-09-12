8195c22f71b631f1274dae2d7074c0ca5f074173
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-09-12 22:01:24 +0800

    列为失效

--- sources/90漫画.js
+++ sources/90漫画.js
@@ -1,118 +1,111 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1652586404,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20230315,
+		minMyACG: 20230911,
 		
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 40,
 		
-		//是否失效，默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isEnabledInvalid: false,
+		enableInvalid: true,
 		
 		//@NonNull 搜索源名称
 		name: "90漫画",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 4,
+		version: 6,
 
 		//搜索源自动同步更新网址
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/90漫画.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/90漫画.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/90漫画.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/90漫画.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/90漫画.js",
 		},
 		
-		//更新时间
-		updateTime: "2023年3月15日",
+		//最近更新时间
+		lastUpdateTime: 1694527150,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 1,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 1,
 		
-		//自定义标签
-		groupName: ["漫画"],
+		//分组
+		group: ["漫画"],
 		
 		//@NonNull 详情页的基本网址
 		baseUrl: baseUrl,
 	});
 }
 const baseUrl = "https://api.90mh.com";
-const header = '';
+const baseImgUrl = "https://js.tingliu.cc";
 
 /**
  * 搜索
  * @param {string} key
- * @return {[{title, summary, coverUrl, url}]}
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = ToolUtils.urlJoin(baseUrl,'/app/comic/search?sort=click&keywords=' + encodeURI(key) + header);
-	var response = HttpRequest(url);
-	var result= [];
+	var url = JavaUtils.urlJoin(baseUrl, '/app/comic/search?sort=click&keywords=' + encodeURI(key));
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		const $ = JSON.parse(response.html())
+		const $ = JSON.parse(response.body().string());
 		$.items.forEach((child) => {
 			result.push({
-				//标题
-				title: child.name,
+				//名称
+				name: child.name,
 		
 				//概览
 				summary: child.last_chapter_name,
 		
 				//封面网址
-				coverUrl: ToolUtils.urlJoin('https://js.tingliu.cc/',child.cover),
+				coverUrl: JavaUtils.urlJoin(baseImgUrl, child.cover),
 		
 				//网址
-				url: ToolUtils.urlJoin(baseUrl,'/app/comic/view?id=' + child.id)
+				url: JavaUtils.urlJoin(baseUrl, '/app/comic/view?id=' + child.id)
 			})
 		})
 	}
 	return JSON.stringify(result);
 }
+
 /**
  * 详情
- * @return {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = HttpRequest(url + header);
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		const $ = JSON.parse(response.html());
+		const $ = JSON.parse(response.body().string());
 		return JSON.stringify({
-			//标题
-			title : $.data.title,
+			//名称
+			name : $.data.title,
 			
 			//作者
 			author: $.data.author,
-			
-			//日期
-			//date: ,
 			
 			//概览
 			summary: $.data.description,
 
 			//封面网址
-			coverUrl: ToolUtils.urlJoin('https://js.tingliu.cc/',$.data.cover),
+			coverUrl: JavaUtils.urlJoin(baseImgUrl, $.data.cover),
 			
-			//是否启用将章节置为倒序
-			isEnabledChapterReverseOrder: false,
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
 			
 			//目录加载
 			tocs: tocs(response)
@@ -129,14 +122,14 @@ function tocs(response) {
 	//创建章节数组
 	var newChapters= [];
 	
-	var chapters = com.jayway.jsonpath.JsonPath.parse(response.html()).read('$.data.chapterGroup.*.*');
+	var chapters = response.body().jsonDocument().read('$.data.chapterGroup.*.*');
 	const $ = JSON.parse(chapters)
 	$.forEach((child) => {
 		newChapters.push({
 			//章节名称
 			name: child.name,
 			//章节网址
-			url: 'https://api.90mh.com/app/chapter/view?id=' + child.id
+			url: JavaUtils.urlJoin(baseUrl, '/app/chapter/view?id=' + child.id)
 		});
 	})
 	return [{
@@ -152,12 +145,12 @@ function tocs(response) {
  * @return {string} content
  */
 function content(url) {
-	const response = HttpRequest(url + header);
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		const $ = JSON.parse(response.html());
+		const $ = JSON.parse(response.body().string());
 		var newImageArray= [];
 		$.data.imageArray.forEach((child) => {
-			newImageArray.push('http://js.tingliu.cc/' + $.data.path + child);
+			newImageArray.push(JavaUtils.urlJoin(baseImgUrl, $.data.path + child));
 		})
 		return JSON.stringify(newImageArray);
 	}
