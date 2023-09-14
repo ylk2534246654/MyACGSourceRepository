77e4ca7c844d88184659a4b698264d5f62b8af87
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-09-14 17:51:38 +0800

    修复并不再兼容 1.4.7 以下版本

--- sources/9x笔趣阁.js
+++ sources/9x笔趣阁.js
@@ -1,27 +1,23 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1660118962,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220101,
+		minMyACG: 20230911,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 10,
 		
-		//是否失效，默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		invalid: false,
+		enableInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "9x笔趣阁",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
@@ -32,126 +28,192 @@ function manifest() {
 
 		//搜索源自动同步更新网址
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/9x笔趣阁.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/9x笔趣阁.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/9x笔趣阁.js",
-			"Coding": "https://ylk2534246654.coding.net/p/myacg/d/MyACGSourceRepository/git/raw/master/sources/9x笔趣阁.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/9x笔趣阁.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/9x笔趣阁.js",
 		},
 		
-		//更新时间
-		updateTime: "2022年8月10日",
+		//最近更新时间
+		lastUpdateTime: 1694681353,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 4,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 1,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 1,
 		
-		//自定义标签
-		tag: ["小说"],
+		//分组
+		group: ["小说"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "https://gfnormal05at.com",//备份：https://nso92.xsafetech.com/
+		baseUrl: baseUrl,
+
+		//发现
+		findList: {
+			category: {
+				"label": {
+					"玄幻奇幻": "1",
+					"武侠仙侠": "2",
+					"都市言情": "3",
+					"历史军事": "4",
+					"科幻灵异": "5",
+					"网游竞技": "6",
+					"女生频道": "7",
+					"同人小说": "66",
+				},
+				"order": {
+					"热门榜": "zuire",
+					"完结榜": "wanjie",
+					"新书榜": "xinshu",
+					"推荐榜": "tuijian",
+				}
+			},
+			"小说": ["label","order"]
+		},
+
+		//全局 HTTP 请求头列表
+		httpRequestHeaderList: {
+			"user-agent-system": "Windows NT 10.0; Win64; x64",
+		}
 	});
 }
 
-const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
+const baseUrl = "https://novel-api.xiaoppkk.com";
+//备份：gfnormal05at.com、nso92.xsafetech.com
+//novel-api.xiaoppkk.com/h5/
+
 /**
  * 搜索
- * @params {string} key
- * @returns {[{title, summary, cover, url}]}
+ * @param {string} key
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = 'https://gfnormal05at.com/h5/search?word='+ encodeURI(key) + header;
-	const response = httpRequest(url);
-	
-	const list = jsoupArray(response,'.books-list > li').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
-			//标题
-			title : jsoup(data,'div > h3').text(),
-			
-			//概览
-			summary : jsoup(data,'p.desc').text(),
-			
-			//封面
-			cover : ToolUtil.urlJoin(url,jsoup(data,'div.cover > a > img').attr('src')),
-			
-			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'div.cover > a').attr('href'))
-			});
-	}
-	return JSON.stringify(array);
-}
-/**
- * 详情
- * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
- */
-function detail(url) {
-	const response = httpRequest(url+ header);
-	return JSON.stringify({
-		//标题
-		title : jsoup(response,'div.info > h3').text(),
+	var url = JavaUtils.urlJoin(baseUrl, `/api/category-search?name=${encodeURI(key)}`);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		JSON.parse(response.body().string()).result.list.forEach((child) => {
+			result.push({
+				//名称
+				name: child.name,
 		
-		//作者
-		author: jsoup(response,'div.info > p.upload').text(),
-		
-		//日期
-		date : jsoup(response,'p.update > span').text(),
-		
-		//概览
-		summary: jsoup(response,'.desc').text(),
+				//作者
+				author: child.author,
 
-		//封面
-		cover : ToolUtil.urlJoin(url,jsoup(response,'div.cover > img').attr('src')),
+				//概览
+				summary: child.description,
+
+				//封面网址
+				coverUrl: child.icon,
 		
-		//目录是否倒序
-		reverseOrder: false,
-		
-		//目录网址/非外链无需使用
-		catalog: catalog(ToolUtil.urlJoin(url,jsoup(response,'div.catalog > div > a').attr('href')))
-	})
-}
-/**
- * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
- */
-function catalog(url) {
-	const response = httpRequest(url+ header);
-	//创建目录数组
-	var new_catalogs= [];
-		
-	//创建章节数组
-	var newchapters= [];
-		
-	//章节代码
-	var chapters = jsoupArray(response,'div.page > ul > li').outerHtml();
-		
-	for (var ci=0;ci<chapters.length;ci++) {
-		var chapter = chapters[ci];
-			
-		newchapters.push({
-			//章节名称
-			name: jsoup(chapter,'a').text(),
-			//章节网址
-			url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
+				//网址
+				url: child.id
+			});
 		});
 	}
-	//添加目录
-	new_catalogs.push({
-		//目录名称
-		tag: '目录',
-		//章节
-		chapter : newchapters
-	});
-	return new_catalogs;
+	return JSON.stringify(result);
+}
+
+/**
+ * 发现
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
+ */
+function find(label, order) {
+	var url = JavaUtils.urlJoin(baseUrl, `/cdn/category/rankList/1/${label}/${order}/all/1.html`);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		JSON.parse(response.body().string()).result.list.forEach((child) => {
+			result.push({
+				//名称
+				name: child.name,
+		
+				//作者
+				author: child.author,
+
+				//概览
+				summary: child.description,
+
+				//封面网址
+				coverUrl: child.icon,
+		
+				//网址
+				url: child.id
+			});
+		});
+	}
+	return JSON.stringify(result);
+}
+
+/**
+ * 详情
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ */
+function detail(id) {
+	//兼容网址
+	if(JavaUtils.isValidUrl(id)){
+		const match = id.match(/book-id-(\d+)/);
+		if (match) {
+			id = match[1];
+		}
+	}
+	var url = JavaUtils.urlJoin(baseUrl, `/api/book-info?id=${id}&source_id=1`);
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		var book = JSON.parse(response.body().string()).result.book;
+		return JSON.stringify({
+			//名称
+			name: book.name,
+			
+			//作者
+			author: book.author,
+			
+			//最近更新时间
+			lastUpdateTime: book.mtime,
+			
+			//概览
+			summary: book.description,
+	
+			//封面网址
+			coverUrl: book.icon,
+			
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(id)
+		});
+	}
+}
+
+/**
+ * 目录
+ * @return {[{name, chapters:{[{name, url}]}}]}
+ */
+function tocs(id) {
+	var url = JavaUtils.urlJoin(baseUrl, `/cdn/book/chapterList/${id}.html`);
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		//创建章节数组
+		var newChapters = [];
+		JSON.parse(response.body().string()).result.list[0].list.forEach((child) => {
+			newChapters.push({
+				//章节名称
+				name: child.name,
+				//章节网址
+				url: child.url
+			});
+		});
+
+		return [{
+			//目录名称
+			name: "目录",
+			//章节
+			chapters : newChapters
+		}];
+	}
+	return null;
 }
 
 /**
@@ -160,7 +222,9 @@ function catalog(url) {
  * @returns {string} content
  */
 function content(url) {
-	const response = httpRequest(url + header);
-	return jsoup(response,'article > section').outerHtml().replace(jsoup(response,'body > article > section > h4').outerHtml(),'')
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		return JSON.parse(response.body().string()).data.content;
+	}
 }
 
