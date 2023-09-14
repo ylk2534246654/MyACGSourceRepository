24f67934688eab955e90e4d8e8d29240fe79e901
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-09-15 00:05:01 +0800

    修复并不再兼容 1.4.7 以下版本

--- sources/唐三中文网.js
+++ sources/唐三中文网.js
@@ -1,22 +1,18 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1654510468,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20230207,
+		minMyACG: 20230911,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 10,
 		
-		//是否失效，默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isInvalid: false,
+		enableInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "唐三中文网",
@@ -28,116 +24,179 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 4,
+		version: 5,
 
 		//搜索源自动同步更新链接
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/唐三中文网.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/唐三中文网.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/唐三中文网.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/唐三中文网.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/唐三中文网.js",
 		},
 		
-		//更新时间
-		updateTime: "2023年1月29日",
+		//最近更新时间
+		lastUpdateTime: 1694705422,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 4,
 		
-		//内容处理方式： -1: 搜索相似，0：对链接处理并调用外部APP访问，1：对链接处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 1,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 1,
 		
-		//自定义标签
+		//分组
 		group: ["小说"],
 		
 		//@NonNull 详情页的基本网址
 		baseUrl: baseUrl,
+
+		//发现
+		findList: {
+			category: {
+				"label": {
+					"玄幻": "1",
+					"仙侠": "2",
+					"都市": "3",
+					"军史": "4",
+					"网游": "5",
+					"科幻": "6",
+				}
+			},
+			"小说": ["label"]
+		},
+
+		//全局 HTTP 请求头列表
+		httpRequestHeaderList: {
+			"user-agent-system": "Windows NT 10.0; Win64; x64"
+		}
 	});
 }
 
-const baseUrl = "http://www.tangsanshu.net";//失效：https://www.xuanshu.com
-const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
+const baseUrl = "http://www.xtangsanshu.com";
+//备份：www.tangsanshu.net、www.xtangsanshu.com、www.xuanshu.com
+
 /**
  * 搜索
- * @params {string} key
- * @returns {[{title, summary, coverUrl, url}]}
+ * @param {string} key
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = ToolUtil.urlJoin(baseUrl,'/s.php?ie=utf-8&q=' + encodeURI(key) + header);
-	const response = httpRequest(url);
-
-	var document = org.jsoup.Jsoup.parse(response,url);
+	var url = JavaUtils.urlJoin(baseUrl, '/s.php?ie=utf-8&q='+ encodeURI(key));
 	var result = [];
-    var elements = document.select("div.so_list > div > div");
-	for (var i = 0;i < elements.size();i++) {
-	    var element = elements.get(i);
-		result.push({
-			//标题
-			title: element.selectFirst('h4.bookname').text(),
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		const elements = document.select("div.so_list > div > div");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('h4.bookname').text(),
+				
+				//作者
+				author: document.selectFirst('.author').text(),
 			
-			//概览
-			summary: element.selectFirst('div.update > a').text(),
-			
-			//封面网址
-			coverUrl: element.selectFirst('div.bookimg > a > img').absUrl('src'),
-			
-			//网址
-			url: element.selectFirst('h4.bookname > a').absUrl('href')
-		});
+				//最后章节名称
+				lastChapterName: element.selectFirst('div.update > a').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('div.bookimg > a > img').absUrl('src'),
+				
+				//网址
+				url: element.selectFirst('h4.bookname > a').absUrl('href')
+			});
+		}
 	}
 	return JSON.stringify(result);
 }
+
+/**
+ * 发现
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
+ */
+function find(label) {
+	var url = JavaUtils.urlJoin(baseUrl, `/fenlei/${label}/1.html/`);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		const elements = document.select(".ll > div");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('dt > span').text(),
+
+				//作者
+				author: element.selectFirst('dt > a').text(),
+
+				//概览
+				summary: element.selectFirst('dl > dd').text(),
+
+				//封面网址
+				coverUrl: element.selectFirst('.image > a > img').absUrl('src'),
+				
+				//网址
+				url: element.selectFirst('.image > a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
 /**
  * 详情
- * @returns {[{title, author, date, summary, coverUrl, isReverseOrder, catalogs:{[{name, chapters:{[{name, url}]}}]}}]}
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = httpRequest(url + header);
-	var document = org.jsoup.Jsoup.parse(response,url);
-	return JSON.stringify({
-		//标题
-		title : document.selectFirst('div.info > h2').text(),
-		
-		//作者
-		author: document.selectFirst('div.info > div.small > span:nth-child(1)').text(),
-		
-		//日期
-		date: document.selectFirst('div.info > div.small > span:nth-child(5)').text(),
-		
-		//概览
-		summary: document.selectFirst('div.intro').text(),
-
-		//封面网址
-		coverUrl: document.selectFirst('div.cover > img').absUrl('src'),
-		
-		//目录是否倒序
-		isReverseOrder: false,
-		
-		//目录网址/非外链无需使用
-		catalogs: catalogs(document)
-	});
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		return JSON.stringify({
+			//名称
+			name: document.selectFirst('div.info > h2').text(),
+			
+			//作者
+			author: document.selectFirst('div.info > div.small > span:nth-child(1)').text(),
+			
+			//最近更新时间
+			lastUpdateTime: document.selectFirst('div.info > div.small > span:nth-child(5)').text(),
+			
+			//概览
+			summary: document.selectFirst('div.intro').text(),
+	
+			//封面网址
+			coverUrl: document.selectFirst('div.cover > img').absUrl('src'),
+			
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
 }
 
 /**
  * 目录
- * @returns {[{name, chapters:{[{name, url}]}}]}
+ * @return {[{name, chapters:{[{name, url}]}}]}
  */
-function catalogs(document) {
+function tocs(document) {
 	//创建章节数组
-	var newChapters= [];
-	
+	var newChapters = [];
+		
 	//章节元素选择器
 	var chapterElements = document.select('div.listmain > dl > dd+dt ~dd');
 	
 	for (var i2 = 0;i2 < chapterElements.size();i2++) {
 		var chapterElement = chapterElements.get(i2);
+		
 		newChapters.push({
-            //章节名称
-            name: chapterElement.text(),
-            //章节网址
-            url: chapterElement.selectFirst('a').absUrl('href')
-        });
+			//章节名称
+			name: chapterElement.selectFirst('a').text(),
+			//章节网址
+			url: chapterElement.selectFirst('a').absUrl('href')
+		});
 	}
 	return [{
 		//目录名称
@@ -149,10 +208,11 @@ function catalogs(document) {
 
 /**
  * 内容
+ * @params {string} url
  * @returns {string} content
  */
 function content(url) {
-	const response = httpRequest(url + header);
+	const response = httpRequest(url);
 	var document = org.jsoup.Jsoup.parse(response,url);
 	return document.select(`#content:not(:matches(唐三中文网|${baseUrl})):matchText`).outerHtml();
 }
--- sources/读一读小说网.js
+++ sources/读一读小说网.js
@@ -1,163 +1,208 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1670506310,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220101,
+		minMyACG: 20230911,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 40,
 		
-		//是否失效，默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		invalid: false,
+		enableInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "读一读小说网",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 1,
+		version: 2,
 
 		//搜索源自动同步更新链接
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/读一读小说网.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/读一读小说网.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/读一读小说网.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/读一读小说网.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/读一读小说网.js",
 		},
 		
-		//更新时间
-		updateTime: "2022年12月8日",
+		//最近更新时间
+		lastUpdateTime: 1694705301,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 4,
 		
-		//内容处理方式： -1: 搜索相似，0：对链接处理并调用外部APP访问，1：对链接处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 1,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 1,
 		
-		//自定义标签
-		tag: ["小说"],
+		//分组
+		group: ["小说"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "http://duyidu.net",//备用：http://du1du.org
+		baseUrl: baseUrl,
+		
+		//发现
+		findList: {
+			category: {
+				"label": {
+					"玄幻": "xuanhuanxiaoshuo",
+					"修真": "xiuzhenxiaoshuo",
+					"都市": "dushixiaoshuo",
+					"穿越": "chuanyuexiaoshuo",
+					"网游": "wangyouxiaoshuo",
+					"科幻": "kehuanxiaoshuo",
+					"言情": "yanqingxiaoshuo",
+					"同人": "tongrenxiaoshuo",
+				}
+			},
+			"小说": ["label"]
+		},
 	});
 }
 
-const header = '';
+const baseUrl = "http://www.b520.cc";
+//备用：duyidu.net、du1du.org
+
 /**
  * 搜索
  * @params {string} key
  * @returns {[{title, summary, cover, url}]}
  */
 function search(key) {
-	var url = 'http://duyidu.net/search.htm?keyword='+ encodeURI(key) + header;
-	const response = httpRequest(url);
-	
-	const list = jsoupArray(response,'#novel-list > ul > li.list-group-item.clearfix:nth-child(n+2)').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
-			//标题
-			title : jsoup(data,'.col-xs-3 > a').text(),
-			
-			//概览
-			summary : jsoup(data,'.col-xs-2:nth-child(4)').text(),
-			
-			//封面
-			//cover : ToolUtil.urlJoin(url,jsoup(data,'.pic > a > img').attr('src')),
-			
-			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'.col-xs-3 > a').attr('href'))
-		});
-	}
-	return JSON.stringify(array);
-}
-/**
- * 详情
- * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
- */
-function detail(url) {
-	const response = httpRequest(url+ header);
-	return JSON.stringify({
-		//标题
-		title : jsoup(response,'meta[property=og:title]').attr('content'),
-		
-		//作者
-		author: jsoup(response,'meta[property=og:novel:author]').attr('content'),
-		
-		//日期
-		date : jsoup(response,'li.col-xs-4:nth-child(4)').text(),
-		
-		//概览
-		summary: jsoup(response,'#all').text(),
+	var url = JavaUtils.urlJoin(baseUrl, '/modules/article/search.php?searchkey=' + encodeURI(key));
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		const elements = document.select("tbody > tr:not([align=center])");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('td:nth-child(1)').text(),
 
-		//封面
-		cover : ToolUtil.urlJoin(url,jsoup(response,'meta[property=og:image]').attr('content')),
-		
-		//目录是否倒序
-		reverseOrder: false,
-		
-		//目录链接/非外链无需使用
-		catalog: catalog(ToolUtil.urlJoin(url,jsoup(response,'a.btn-danger').attr('href')))
-	})
-}
-/**
- * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
- */
-function catalog(url) {
-	const response = httpRequest(url + header);
-	
-	//创建目录数组
-	var new_catalogs= [];
-		
-	//创建章节数组
-	var newchapters= [];
-		
-	//章节代码
-	var chapters = jsoupArray(response,'#chapters-list > li').outerHtml();
-		
-	var volumn = '';
-	for (var ci=0;ci<chapters.length;ci++) {
-		var chapter = chapters[ci];
-		
-		if(chapter.indexOf('volumn') != -1){
-			volumn = jsoup(chapter,'.volumn').text();
-		}else{
-			newchapters.push({
-				//章节名称
-				name: volumn + ' ' + jsoup(chapter,'a').text(),
-				//章节链接
-				url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
+				//作者
+				author: element.selectFirst('td:nth-child(3)').text(),
+
+				//最后章节名称
+				lastChapterName: element.selectFirst('td:nth-child(2)').text(),
+				
+				//最近更新时间
+				lastUpdateTime: element.selectFirst('td:nth-child(5)').text(),
+				
+				//封面网址
+				//coverUrl: element.selectFirst('').absUrl('data-original'),
+				
+				//网址
+				url: element.selectFirst('td:nth-child(1) > a').absUrl('href')
 			});
 		}
-		
 	}
-	//添加目录
-	new_catalogs.push({
+	return JSON.stringify(result);
+}
+
+/**
+ * 发现
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
+ */
+function find(label) {
+	var url = JavaUtils.urlJoin(baseUrl, `/${label}/`);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		const elements = document.select(".ll > div");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('dt > span').text(),
+
+				//作者
+				author: element.selectFirst('dt > a').text(),
+
+				//概览
+				summary: element.selectFirst('dl > dd').text(),
+
+				//封面网址
+				coverUrl: element.selectFirst('.image > a > img').absUrl('src'),
+				
+				//网址
+				url: element.selectFirst('.image > a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
+/**
+ * 详情
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ */
+function detail(url) {
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		return JSON.stringify({
+			//名称
+			name: document.selectFirst('meta[property="og:title"]').attr('content'),
+			
+			//作者
+			author: document.selectFirst('meta[property="og:novel:author"]').attr('content'),
+			
+			//最近更新时间
+			lastUpdateTime: document.selectFirst('#info > p:nth-child(5)').text(),
+			
+			//概览
+			summary: document.selectFirst('#intro').text(),
+	
+			//封面网址
+			coverUrl: document.selectFirst('meta[property="og:image"]').absUrl('content'),
+			
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
+}
+
+/**
+ * 目录
+ * @returns {[{name, chapters:{[{name, url}]}}]}
+ */
+function tocs(document) {
+	//创建章节数组
+	var newChapters= [];
+	
+	//章节元素选择器
+	var chapterElements = document.select('#list> dl > dd+dt ~dd');
+	
+	for (var i2 = 0;i2 < chapterElements.size();i2++) {
+		var chapterElement = chapterElements.get(i2);
+		newChapters.push({
+            //章节名称
+            name: chapterElement.text(),
+            //章节网址
+            url: chapterElement.selectFirst('a').absUrl('href')
+        });
+	}
+	return [{
 		//目录名称
-		tag: '目录',
+		name: "目录",
 		//章节
-		chapter : newchapters
-	});
-	return new_catalogs;
+		chapters: newChapters
+	}];
 }
 
 /**
@@ -166,8 +211,10 @@ function catalog(url) {
  * @returns {string} content
  */
 function content(url) {
-	const response = httpRequest(url + header);
-	const src = jsoup(response,'#txtContent').outerHtml();
-	return src;
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		return document.select(`#content:not(:matches(唐三中文网|${baseUrl}))`).outerHtml();
+	}
+	return null;
 }
-
