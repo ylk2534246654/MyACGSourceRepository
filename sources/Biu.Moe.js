94beb0095742c024f18a4c2223372a71f0255074
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-09-19 20:09:03 +0800

    不再兼容 1.4.7 以下版本

--- sources/Biu.Moe.js
+++ sources/Biu.Moe.js
@@ -1,22 +1,18 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1652603756,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220101,
+		minMyACG: 20230911,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 20,
 		
-		//是否失效，默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		invalid: false,
+		enableInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "Biu.Moe",
@@ -28,72 +24,135 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 1,
+		version: 2,
 
 		//搜索源自动同步更新网址
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/Biu.Moe.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/Biu.Moe.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/Biu.Moe.js",
-			"Coding": "https://ylk2534246654.coding.net/p/myacg/d/MyACGSourceRepository/git/raw/master/sources/Biu.Moe.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/Biu.Moe.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/Biu.Moe.js",
 		},
 		
-		//更新时间
-		updateTime: "2022年3月29日",
+		//最近更新时间
+		lastUpdateTime: 1695114620,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 5,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 1,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 1,
 		
-		//自定义标签
-		tag: ["音乐"],
+		//分组
+		group: ["音乐"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "https://web.biu.moe",
+		baseUrl: baseUrl,
+
+		//发现
+		findList: {
+			category: {
+				"label": {
+					"未分类": "0",
+					"动漫": "1",
+					"Galgame": "2",
+					"偶像": "3",
+					"东方Project": "4",
+					"VOCALOID": "5",
+					"同人": "6",
+					"纯音乐": "7",
+				},
+			},
+			"音乐": ["label"]
+		},
 	});
 }
-const header = '';
+
+const baseUrl = "https://web.biu.moe";
 
 /**
  * 搜索
- * @params {string} key
- * @returns {[{title, summary, cover, url}]}
+ * @param {string} key
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = 'https://web.biu.moe/index.php?m=&c=Song&a=search&data='+ encodeURI(key) + header;
-	const response = httpRequest(url);
-	
-	const list = jsoupArray(response,'tbody > tr').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
-			//标题
-			title : jsoup(data,'a:nth-child(1)').text(),
-			
-			//概览
-			summary : jsoup(data,'a:nth-child(2)').text(),
-			
-			//封面
-			//cover : ToolUtil.urlJoin(url,jsoup(data,'p:nth-child(1) > a > img').attr('src')),
-			
-			//网址
-			url : ToolUtil.urlJoin('https://web.biu.moe/Song/playSID/sid/',jsoup(data,'a:nth-child(1)').attr('href').replace('/s',''))
+	var url = JavaUtils.urlJoin(baseUrl, '/index.php?m=&c=Song&a=search&data='+ encodeURI(key));
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		const elements = document.select("tbody > tr");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			var match = element.selectFirst('td:nth-child(1) > a').absUrl('href').match(/s(\d+)/);
+			var id;
+			if (match) {
+				id = match[1];
+			}
+			result.push({
+				//名称
+				name: element.selectFirst('td:nth-child(1) > a').text().replace(element.selectFirst('td:nth-child(1) > a > span').text(),''),
+				
+				//作者
+				author: element.selectFirst('td:nth-child(2) > a').text(),
+				
+				//封面网址
+				coverUrl: JavaUtils.urlJoin(baseUrl, `/Song/showCover/sid/${id}`),
+				
+				//网址
+				url: JavaUtils.urlJoin(baseUrl, `/Song/playSID/sid/${id}`),
 			});
+		}
 	}
-	return JSON.stringify(array);
+	return JSON.stringify(result);
 }
+
+
+/**
+ * 发现
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
+ */
+function find(label) {
+	var url = JavaUtils.urlJoin(baseUrl, `/Index/type/t/${label}`);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		const elements = document.select("tbody > tr");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			var match = element.selectFirst('td:nth-child(1) > a').absUrl('href').match(/s(\d+)/);
+			var id;
+			if (match) {
+				id = match[1];
+			}
+			result.push({
+				//名称
+				name: element.selectFirst('td:nth-child(1) > a').text().replace(element.selectFirst('td:nth-child(1) > a > span').text(),''),
+				
+				//作者
+				author: element.selectFirst('td:nth-child(2) > a').text(),
+				
+				//封面网址
+				coverUrl: JavaUtils.urlJoin(baseUrl, `/Song/showCover/sid/${id}`),
+				
+				//网址
+				url: JavaUtils.urlJoin(baseUrl, `/Song/playSID/sid/${id}`),
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
 /**
  * 内容
- * @params {string} url
- * @returns {string} url
+ * @param {string} url
+ * @return {string} url
  */
 function content(url) {
-	const response = httpRequest(url);
-	return jsonPath(response,'$.urlinfo.url');
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		return JSON.parse(response.body().string()).urlinfo.url;
+	}
 }
 
