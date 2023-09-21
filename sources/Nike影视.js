0ddc8ba6da31ff13bcfdbd9522bde504eeba8f99
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-09-22 02:19:23 +0800

    修复并不再兼容 1.4.7 以下版本

--- sources/Nike影视.js
+++ sources/Nike影视.js
@@ -1,27 +1,23 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1660663675,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20230317,
+		minMyACG: 20230911,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 35,
 		
-		//是否启用失效#默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isEnabledInvalid: false,
+		enableInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "Nike影视",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
@@ -32,57 +28,54 @@ function manifest() {
 
 		//搜索源自动同步更新网址
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/Nike影视.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/Nike影视.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/Nike影视.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/Nike影视.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/Nike影视.js",
 		},
 		
-		//更新时间
-		updateTime: "2023年4月27日",
+		//最近更新时间
+		lastUpdateTime: 1695299080,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 2,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 2,
 		
-		//自定义标签，第一个标签作为发现分类
-		tag: ["影视","动漫"],
+		//分组
+		group: ["影视","动漫"],
 		
 		//@NonNull 详情页的基本网址
 		baseUrl: baseUrl,//和动漫星球结构相似，站长：ajeee.com@gmail.com
-		
-		//发现
-		findList: {
-			"最近更新": "/label/new.html",
-		},
 	});
 }
+
 const baseUrl = "https://www.ajeee.com";
-const header = '';
 
 /**
  * 搜索
  * @param {string} key
- * @return {[{title, summary, coverUrl, url}]}
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = ToolUtils.urlJoin(baseUrl,'/search.html?wd=' + encodeURI(key));
-	const response = HttpRequest(url + header);
-	var result= [];
+	var url = JavaUtils.urlJoin(baseUrl, '/search.html?wd=' + encodeURI(key));
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		var document = response.document();
+		const document = response.body().cssDocument();
 		var elements = document.select("div.module-items > div");
 		for (var i = 0;i < elements.size();i++) {
 			var element = elements.get(i);
 			result.push({
-				//标题
-				title: element.selectFirst('div.module-card-item-title > a').text(),
+				//名称
+				name: element.selectFirst('div.module-card-item-title > a').text(),
 				
+				//最后章节名称
+				lastChapterName: element.selectFirst('.module-item-note').text(),
+
 				//概览
-				summary: element.selectFirst('div.module-info-item-content').text(),
+				summary: element.selectFirst('.module-card-item-info > div:nth-child(3) > div').text(),
 				
 				//封面网址
 				coverUrl: element.selectFirst('img[data-original]').absUrl('data-original'),
@@ -94,54 +87,24 @@ function search(key) {
 	}
 	return JSON.stringify(result);
 }
-/**
- * 发现
- * @param string url
- * @return {[{title, summary, coverUrl, url}]}
- */
-function find(url) {
-	url = ToolUtils.urlJoin(baseUrl, url);
-	const response = HttpRequest(url + header);
-	var result= [];
-	if(response.code() == 200){
-		var document = response.document();
-		var elements = document.select(".module-main > div > a");
-		for (var i = 0;i < elements.size();i++) {
-			var element = elements.get(i);
-			result.push({
-				//标题
-				title: element.selectFirst('.module-poster-item-title').text(),
-				
-				//概览
-				summary: element.selectFirst('.module-item-note').text(),
-				
-				//封面网址
-				coverUrl: element.selectFirst('.module-item-pic > img').absUrl('data-original'),
-				
-				//网址
-				url: element.selectFirst('a').absUrl('href')
-			});
-		}
-	}
-	return JSON.stringify(result);
-}
+
 /**
  * 详情
- * @return {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = HttpRequest(url + header);
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		var document = response.document();
+		const document = response.body().cssDocument();
 		return JSON.stringify({
 			//标题
-			title: document.selectFirst('div.module-info-heading > h1').text(),
+			name: document.selectFirst('div.module-info-heading > h1').text(),
 			
 			//作者
-			author: document.selectFirst('div:nth-child(2) > div.module-info-item-content > a').text(),
+			//author: document.selectFirst('div:nth-child(2) > div.module-info-item-content > a').text(),
 			
-			//更新时间
-			//update: document.selectFirst('').text(),
+			//最近更新时间
+			//lastUpdateTime: document.selectFirst('.module-info-content > div > div:nth-child(8) > div').text(),
 			
 			//概览
 			summary: document.selectFirst('div.module-info-introduction > div > p').text(),
@@ -149,8 +112,8 @@ function detail(url) {
 			//封面网址
 			coverUrl: document.selectFirst('div.module-info-poster > div > div > img').absUrl('data-original'),
 			
-			//是否启用将章节置为倒序
-			isEnabledChapterReverseOrder: false,
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
 			
 			//目录加载
 			tocs: tocs(document)
@@ -158,13 +121,14 @@ function detail(url) {
 	}
 	return null;
 }
+
 /**
  * 目录
  * @return {[{name, chapters:{[{name, url}]}}]}
  */
 function tocs(document) {
 	//目录元素选择器
-	const tocElements= document.select('div.module-list');
+	const tocElements = document.select('div.module-list');
 	
 	//创建目录数组
 	var newTocs = [];
@@ -183,7 +147,7 @@ function tocs(document) {
 				//章节名称
 				name: chapterElement.selectFirst('a').text(),
 				//章节网址
-				url: chapterElement.selectFirst('a').absUrl('href') + header
+				url: chapterElement.selectFirst('a').absUrl('href')
 			});
 		}
 		newTocs.push({
@@ -198,14 +162,14 @@ function tocs(document) {
 
 /**
  * 内容（部分搜索源通用过滤规则）
- * @version 2023/4/27
- * 布米米、嘻嘻动漫、12wo动漫、路漫漫、风车动漫P、樱花动漫P、COCO漫画、Nike
+ * @version 2023/9/21
+ * 布米米、嘻嘻动漫、12wo动漫、路漫漫、风车动漫P、樱花动漫P、COCO漫画、Nike、cocoManga
  * @return {string} content
  */
 function content(url) {
 	var re = new RegExp(
 		//https://
-		'[a-z]+://[\\w.:]+/(' +
+		'[a-zA-z]+://[^\\s/]+/(' +
 
 		//https://knr.xxxxx.cn/j/140000		#[a-z]{1}\/\d{6}
 		'([a-z]{1}/\\d)' +
@@ -235,7 +199,8 @@ function content(url) {
 		//https://xxxx.xxxx.xx:00000/kmopef/3.woff # [\w/]+[/km][\w/]+\.woff
 		'|([\\w/]+[/km][\\w/]+\\.woff)' +
 
-		')'
+		')'+
+		''
 		,
 		'i'
 	);
--- sources/爱米推漫画.js
+++ sources/爱米推漫画.js
@@ -1,165 +1,167 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1678939681,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20230317,
+		minMyACG: 20230911,
 		
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 1,
 		
-		//是否启用失效#默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isEnabledInvalid: false,
+		enableInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "爱米推漫画",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 1,
+		version: 2,
 
 		//搜索源自动同步更新网址
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/爱米推漫画.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/爱米推漫画.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/爱米推漫画.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/爱米推漫画.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/爱米推漫画.js",
 		},
 		
-		//更新时间
-		updateTime: "2023年3月17日",
+		//最近更新时间
+		lastUpdateTime: 1695299902,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 1,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 1,
 		
-		//自定义标签
-		groupName: ["漫画"],
+		//分组
+		group: ["漫画"],
 		
 		//@NonNull 详情页的基本网址
 		baseUrl: baseUrl,
 		
 		//发现
 		findList: {
-			"完结": "/list/wanjie/",
-			"都市": "/list/dushi/",
-			"后宫": "/list/hougong/",
-			"穿越": "/list/chuanyue/"
+			"漫画": {
+				"完结": "/list/wanjie/",
+				"都市": "/list/dushi/",
+				"后宫": "/list/hougong/",
+				"穿越": "/list/chuanyue/"
+			}
 		},
 	});
 }
-const baseUrl = "https://m.imitui.com";
-const header = '';
+
+const baseUrl = "https://m.pinmh.com/";
+//备份：m.pinmh.com,m.imitui.com
 
 /**
  * 搜索
  * @param {string} key
- * @return {[{title, summary, coverUrl, url}]}
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = ToolUtils.urlJoin(baseUrl,'/search/?keywords=' + encodeURI(key) + header);
-	const response = HttpRequest(url);
+	var url = JavaUtils.urlJoin(baseUrl, '/search/?keywords=' + encodeURI(key));
 	var result = [];
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		var document = response.document();
-        var elements = document.select("#update_list > div > div");
-        for (var i = 0;i < elements.size();i++) {
-            var element = elements.get(i);
-            result.push({
-                //标题
-                title: element.selectFirst('div.itemTxt > a').text(),
-                
-                //概览
-                summary: element.selectFirst('a.coll').text(),
-                
-                //封面网址
-                coverUrl: element.selectFirst('div.itemImg > a > mip-img,.itemImg > a > img').absUrl('src'),
-                
-                //网址
-                url: element.selectFirst('div.itemTxt > a').absUrl('href')
-            });
-        }
-    }
+		const document = response.body().cssDocument();
+		var elements = document.select("#update_list > div > div");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('div.itemTxt > a').text(),
+				
+				//最后章节名称
+				lastChapterName: element.selectFirst('a.coll').text(),
+
+				//最近更新时间
+				lastUpdateTime: element.selectFirst('.date').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('div.itemImg > a > mip-img,.itemImg > a > img').absUrl('src'),
+				
+				//网址
+				url: element.selectFirst('div.itemTxt > a').absUrl('href')
+			});
+		}
+	}
 	return JSON.stringify(result);
 }
+
 /**
  * 发现
- * @param {string} url
- * @return {[{title, summary, coverUrl, url}]}
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function find(url) {
-	var url = ToolUtils.urlJoin(baseUrl, url);
-	const response = HttpRequest(url + header);
+	var url = JavaUtils.urlJoin(baseUrl, url);
 	var result = [];
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		var document = response.document();
-        var elements = document.select(".list-comic");
-        for (var i = 0;i < elements.size();i++) {
-            var element = elements.get(i);
-            result.push({
-                //标题
-                title: element.selectFirst('a.txtA').text(),
-                
-                //概览
-                summary: element.selectFirst('span.info').text(),
-                
-                //封面网址
-                coverUrl: element.selectFirst('mip-img,.ImgA > img').absUrl('src'),
-                
-                //网址
-                url: element.selectFirst('a.ImgA').absUrl('href')
-            });
-        }
-    }
+		const document = response.body().cssDocument();
+		var elements = document.select(".list-comic");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('a.txtA').text(),
+				
+                //作者
+            	author: element.selectFirst('span.info').text(),
+
+				//封面网址
+				coverUrl: element.selectFirst('mip-img,.ImgA > img').absUrl('src'),
+				
+				//网址
+				url: element.selectFirst('a.ImgA').absUrl('href')
+			});
+		}
+	}
 	return JSON.stringify(result);
 }
+
 /**
  * 详情
- * @return {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
-	const response = HttpRequest(url + header);
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		var document = response.document();
+		const document = response.body().cssDocument();
         var comicName = document.selectFirst('h1.title').text();
-        return JSON.stringify({
-            //标题
-            title: document.selectFirst('h1.title,#comicName').text(),
-            
+		return JSON.stringify({
+			//标题
+			name: document.selectFirst('h1.title,#comicName').text(),
+			
             //作者
             author: document.selectFirst('div.comic-view.clearfix > div.view-sub.autoHeight > div > dl:nth-child(3) > dd,div.sub_r > p:nth-child(1)').text(),
             
-            //日期
-            update: document.selectFirst('dl:nth-child(5) > dd,span.date').text(),
-            
-            //概览
-            summary: document.selectFirst('div.comic-view.clearfix > p,#simple-des').text(),
-    
-            //封面网址
-            coverUrl: document.selectFirst('div.img > mip-img,#Cover > img').absUrl('src'),
-            
-            //是否启用将章节置为倒序
-            isEnabledChapterReverseOrder: comicName != null && comicName.length > 0,
-            
-            //目录加载
-            tocs: tocs(document)
-        });
-    }
+			//最近更新时间
+			lastUpdateTime: document.selectFirst('dl:nth-child(5) > dd,span.date').text(),
+			
+			//概览
+			summary: document.selectFirst('div.comic-view.clearfix > p,#simple-des').text(),
+	
+			//封面网址
+			coverUrl: document.selectFirst('div.img > mip-img,#Cover > img').absUrl('src'),
+			
+			//启用章节反向顺序
+			enableChapterReverseOrder: comicName != null && comicName.length > 0,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
 	return null;
 }
 
@@ -190,7 +192,7 @@ function tocs(document) {
 				//章节名称
 				name: chapterElement.selectFirst('a').text(),
 				//章节网址
-				url: chapterElement.selectFirst('a').absUrl('href').replace('.html','-${p}.html@zero->1@start->1')
+				url: chapterElement.selectFirst('a').absUrl('href').replace('.html','-{1}.html')
 			});
 		}
 		newTocs.push({
@@ -210,40 +212,41 @@ function tocs(document) {
  * @return {string} content
  */
 function content(url) {
-	const response = HttpRequest(url + header);
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		var document = response.document();
-        var imgList = [];
-	
-        var elements = document.select('\
-mip-link > img:not([style=display: none;]),\
-div.UnderPag > mip-img,\
-div.erPag > mip-img,\
-mip-link > mip-img:not([style=display: none;]),\
-div:not([style]) > mip-link > mip-img:not([style],[width]),\
-mip-link > mip-img,\
-#image,\
-#scroll-image > div > [src],\
-#scroll-image > div > [data-src]');
-        if(elements != null){
-            for (var i = 0;i<elements.size();i++) {
-                var element = elements.get(i);
-                var src = element.absUrl('data-src');
-                if(String(src).length <= 0){
-                    src = element.absUrl('src');
-                }
-                imgList.push(src);
-            }
-        }
-    
-        var newImgList = [];
-        for(var i = 0;i < imgList.length;i++){
-            var re = /default|cover|\.gif|\/manhua\//i;
-            if(!re.test(imgList[i])){
-                newImgList.push(imgList[i]);
-            }
-        }
-        return JSON.stringify(newImgList);
-    }
-    return null;
+		const document = response.body().cssDocument();
+		if(response.code() == 200){
+			var imgList = [];
+		
+			var elements = document.select('\
+	mip-link > img:not([style=display: none;]),\
+	div.UnderPag > mip-img,\
+	div.erPag > mip-img,\
+	mip-link > mip-img:not([style=display: none;]),\
+	div:not([style]) > mip-link > mip-img:not([style],[width]),\
+	mip-link > mip-img,\
+	#image,\
+	#scroll-image > div > [src],\
+	#scroll-image > div > [data-src]');
+			if(elements != null){
+				for (var i = 0;i<elements.size();i++) {
+					var element = elements.get(i);
+					var src = element.absUrl('data-src');
+					if(String(src).length <= 0){
+						src = element.absUrl('src');
+					}
+					imgList.push(src);
+				}
+			}
+		
+			var newImgList = [];
+			for(var i = 0;i < imgList.length;i++){
+				var re = /default|cover|\.gif|\/manhua\//i;
+				if(!re.test(imgList[i])){
+					newImgList.push(imgList[i]);
+				}
+			}
+			return JSON.stringify(newImgList);
+		}
+	}
 }
