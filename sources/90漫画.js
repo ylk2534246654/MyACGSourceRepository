b1b021da698a91836992a9ecba0f356575f6fc38
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-09-24 17:55:20 +0800

    修复并不再兼容 1.4.7 以下版本

--- sources/90漫画.js
+++ sources/90漫画.js
@@ -12,7 +12,7 @@ function manifest() {
 		
 		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		enableInvalid: true,
+		enableInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "90漫画",
@@ -24,7 +24,7 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 6,
+		version: 7,
 
 		//搜索源自动同步更新网址
 		syncList: {
@@ -35,7 +35,7 @@ function manifest() {
 		},
 		
 		//最近更新时间
-		lastUpdateTime: 1694527150,
+		lastUpdateTime: 1695547967,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
@@ -48,10 +48,50 @@ function manifest() {
 		
 		//@NonNull 详情页的基本网址
 		baseUrl: baseUrl,
+
+		//发现
+		findList: {
+			category: {
+				"region": {
+					"全部": "",
+					"日本": "riben",
+					"大陆": "dalu",
+					"韩国": "hanguo"
+				},
+				"label": {
+					"全部": "",
+					"爱情": "aiqing",
+					"魔法": "mofa",
+					"校园": "xiaoyuan",
+					"恐怖": "kongbu",
+					"脑洞": "naodong",
+					"灵异": "lingyi",
+					"恋爱": "lianai",
+				},
+				"status": {
+					"全部": "",
+					"连载中": "lianzai",
+					"已完结": "wanjie",
+				},
+				"order": {
+					"更新排序": "update",
+					"发布排序": "post",
+					"点击排序": "click",
+				},
+			},
+			"动漫": ["region","label","status","order"]
+		},
+
+		//全局 HTTP 请求头列表
+		httpRequestHeaderList: {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+		}
 	});
 }
-const baseUrl = "https://api.90mh.com";
-const baseImgUrl = "https://js.tingliu.cc";
+
+const baseUrl = "http://www.90mh.org";
+const imgBaseUrl = "http://js.tingliu.cc";
+//http://www.90mh.org/js/config.js
 
 /**
  * 搜索
@@ -59,26 +99,71 @@ const baseImgUrl = "https://js.tingliu.cc";
  * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = JavaUtils.urlJoin(baseUrl, '/app/comic/search?sort=click&keywords=' + encodeURI(key));
+	var url = JavaUtils.urlJoin(baseUrl, `/search/?keywords=${encodeURI(key)}`);
 	var result = [];
 	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		const $ = JSON.parse(response.body().string());
-		$.items.forEach((child) => {
+		const document = response.body().cssDocument();
+		var elements = document.select(".book-list > li");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
 			result.push({
 				//名称
-				name: child.name,
-		
+				name: element.selectFirst('.ell').text(),
+				
+				//最后章节名称
+				lastChapterName: element.selectFirst('.tt').text(),
+
+				//最近更新时间
+				lastUpdateTime: element.selectFirst('.updateon').text(),
+
 				//概览
-				summary: child.last_chapter_name,
-		
+				//summary: element.selectFirst('').text(),
+				
 				//封面网址
-				coverUrl: JavaUtils.urlJoin(baseImgUrl, child.cover),
-		
+				coverUrl: element.selectFirst('.cover > img').absUrl('src'),
+				
 				//网址
-				url: JavaUtils.urlJoin(baseUrl, '/app/comic/view?id=' + child.id)
-			})
-		})
+				url: element.selectFirst('.ell > a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
+/**
+ * 发现
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
+ */
+function find(region, label, status, order) {
+	var url = JavaUtils.urlJoin(baseUrl, `/list/${region}-${label}-${status}/${order}/`);
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		const elements = document.select(".book-list > li");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('.ell').text(),
+				
+				//最后章节名称
+				lastChapterName: element.selectFirst('.tt').text(),
+
+				//最近更新时间
+				lastUpdateTime: element.selectFirst('.updateon').text(),
+
+				//概览
+				//summary: element.selectFirst('').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('.cover > img').absUrl('src'),
+				
+				//网址
+				url: element.selectFirst('.ell > a').absUrl('href')
+			});
+		}
 	}
 	return JSON.stringify(result);
 }
@@ -90,25 +175,28 @@ function search(key) {
 function detail(url) {
 	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		const $ = JSON.parse(response.body().string());
+		const document = response.body().cssDocument();
 		return JSON.stringify({
-			//名称
-			name : $.data.title,
+			//标题
+			name: document.selectFirst('.book-title > h1 > span').text(),
 			
 			//作者
-			author: $.data.author,
+			//author: document.selectFirst('').text(),
+			
+			//最近更新时间
+			lastUpdateTime: document.selectFirst('.status > span > span').text(),
 			
 			//概览
-			summary: $.data.description,
-
+			summary: document.selectFirst('#intro-all > p').text(),
+	
 			//封面网址
-			coverUrl: JavaUtils.urlJoin(baseImgUrl, $.data.cover),
+			coverUrl: document.selectFirst('.pic').absUrl('src'),
 			
 			//启用章节反向顺序
 			enableChapterReverseOrder: false,
 			
 			//目录加载
-			tocs: tocs(response)
+			tocs: tocs(document)
 		});
 	}
 	return null;
@@ -116,22 +204,25 @@ function detail(url) {
 
 /**
  * 目录
- * @return {[{name, chapters:{[{name, url}]}}]}
+ * @returns {[{name, chapters:{[{name, url}]}}]}
  */
-function tocs(response) {
+function tocs(document) {
 	//创建章节数组
-	var newChapters= [];
+	var newChapters = [];
+		
+	//章节元素选择器
+	var chapterElements = document.select('[id~=chapter-list] > li');
 	
-	var chapters = response.body().jsonDocument().read('$.data.chapterGroup.*.*');
-	const $ = JSON.parse(chapters)
-	$.forEach((child) => {
+	for (var i2 = 0;i2 < chapterElements.size();i2++) {
+		var chapterElement = chapterElements.get(i2);
+		
 		newChapters.push({
 			//章节名称
-			name: child.name,
+			name: chapterElement.selectFirst('a').text(),
 			//章节网址
-			url: JavaUtils.urlJoin(baseUrl, '/app/chapter/view?id=' + child.id)
+			url: chapterElement.selectFirst('a').absUrl('href')
 		});
-	})
+	}
 	return [{
 		//目录名称
 		name: "目录",
@@ -142,17 +233,14 @@ function tocs(response) {
 
 /**
  * 内容
- * @return {string} content
+ * @params {string} url
+ * @returns {string} content
  */
 function content(url) {
 	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		const $ = JSON.parse(response.body().string());
-		var newImageArray= [];
-		$.data.imageArray.forEach((child) => {
-			newImageArray.push(JavaUtils.urlJoin(baseImgUrl, $.data.path + child));
-		})
-		return JSON.stringify(newImageArray);
+		eval(String(JavaUtils.substring(response.body().string(), '<script>;var', '</script>')));
+		var images = chapterImages.map(value => JavaUtils.urlJoin(imgBaseUrl, chapterPath  + value));
+		return JSON.stringify(images);
 	}
-	return null;
 }
