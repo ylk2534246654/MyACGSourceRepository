fb93adcc1ded26c2369dd754acec3a7064436a2a
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-12-24 14:54:28 +0800

    修复并不再兼容 20230911 以下版本

--- sources/动漫花园.js
+++ sources/动漫花园.js
@@ -1,62 +1,55 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1658157437,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220718,
+		minMyACG: 20231215,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 20,
 		
-		//是否失效，默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		invalid: false,
+		enableInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "动漫花园",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 1,
+		version: 2,
 
 		//搜索源自动同步更新网址
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/动漫花园.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/动漫花园.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/动漫花园.js",
-			"Coding": "https://ylk2534246654.coding.net/p/myacg/d/MyACGSourceRepository/git/raw/master/sources/动漫花园.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/动漫花园.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/动漫花园.js",
 		},
 		
-		//更新时间
-		updateTime: "2022年7月18日",
+		//最近更新时间
+		lastUpdateTime: 1703399974,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 1,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 0,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 0,
 		
-		//自定义标签
-		tag: ["磁力"],
+		//分组
+		group: ["磁力"],
 		
 		//@NonNull 详情页的基本网址
 		baseUrl: "https://dandanplay-api.933.moe",
 	});
 }
-const header = '';
 
 /**
  * 搜索
@@ -64,27 +57,29 @@ const header = '';
  * @returns {[{title, summary, cover, url}]}
  */
 function search(key) {
-	var url = `https://dandanplay-api.933.moe/list?keyword=${encodeURI(key)}` + header;
-	const response = httpRequest(url);
-	var array= [];
-	const $ = JSON.parse(response)
-	$.Resources.forEach((child) => {
-		array.push({
-		//标题
-		title: child.SubgroupName,
-		
-		//概览
-		summary: child.Title,
-		
-		//封面
-		cover: '',
-		
-		//外部网址
-		outerUrl: child.PageUrl,
-		
-		//网址
-		url: child.Magnet
-		})
-	  })
-	return JSON.stringify(array);
+	var result = [];
+	var url = `https://dandanplay-api.933.moe/list?keyword=${encodeURI(key)}`;
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const $ = JSON.parse(response.body().string())
+		$.Resources.forEach((child) => {
+			result.push({
+				//名称
+				name: child.SubgroupName,
+				
+				//概览
+				summary: child.Title,
+				
+				//封面网址
+				//coverUrl: '',
+				
+				//外部网址
+				outerUrl: child.PageUrl,
+				
+				//网址
+				url: child.Magnet
+			});
+		});
+	}
+	return JSON.stringify(result);
 }
