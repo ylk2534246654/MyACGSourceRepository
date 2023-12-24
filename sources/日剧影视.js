de12ccfafeac2dfe6c243e217685d7492f6f375b
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-12-24 19:05:59 +0800

    Add files via upload

--- sources/日剧影视.js
+++ sources/日剧影视.js
@@ -1,27 +1,23 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
-		
 		//@NonNull 搜索源ID标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1660901047,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20230317,
+		minMyACG: 20231215,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 30,
 		
-		//是否启用失效#默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isEnabledInvalid: true,
+		enableInvalid: true,
 		
 		//@NonNull 搜索源名称
 		name: "日剧影视",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
@@ -32,24 +28,23 @@ function manifest() {
 
 		//搜索源自动同步更新链接
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/日剧影视.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/日剧影视.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/日剧影视.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/日剧影视.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/日剧影视.js",
 		},
 		
-		//更新时间
-		updateTime: "2023年3月17日",
+		//最近更新时间
+		lastUpdateTime: 1703407068,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
 		
-		//内容处理方式： 0：链接处理并浏览器访问{url}，1：链接处理{url}，2：浏览器拦截请求{url}，3：浏览器拦截框架{html}
-		contentType: 2,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 2,
 		
-		//自定义标签
-		groupName: ["动漫","影视"],
+		//分组
+		group: ["动漫","影视"],
 		
 		//@NonNull 详细界面的基本网址
 		baseUrl: baseUrl,
@@ -63,8 +58,8 @@ const header = '';
 
 /**
  * 搜索
- * @params {string} key
- * @returns {[{title, summary, cover, url}]}
+ * @param {string} key
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
 	var url = ToolUtils.urlJoin(baseUrl,'/search/-------------.html?wd=' + encodeURI(key));
@@ -76,8 +71,8 @@ function search(key) {
 		for (var i = 0;i < elements.size();i++) {
 			var element = elements.get(i);
 			result.push({
-				//标题
-				title: element.selectFirst('.title').text(),
+				//名称
+				name: element.selectFirst('.title').text(),
 				
 				//概览
 				summary: element.selectFirst('.pic-text').text(),
@@ -92,9 +87,10 @@ function search(key) {
 	}
 	return JSON.stringify(result);
 }
+
 /**
- * 详情
- * @return {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ * 发现
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function detail(url) {
 	const response = HttpRequest(url + header);
@@ -102,13 +98,13 @@ function detail(url) {
 		var document = response.document();
 		return JSON.stringify({
 			//标题
-			title: document.selectFirst('h1.title').text(),
+			name: document.selectFirst('h1.title').text(),
 			
 			//作者
 			//author: document.selectFirst('').text(),
 			
 			//更新时间
-			//update: document.selectFirst('').text(),
+			//lastUpdateTime: document.selectFirst('').text(),
 			
 			//概览
 			summary: document.selectFirst('p.data.hidden-xs > :matchText').text(),
@@ -116,8 +112,8 @@ function detail(url) {
 			//封面网址
 			coverUrl: document.selectFirst('.lazyload').absUrl('data-original'),
 			
-			//是否启用将章节置为倒序
-			isEnabledChapterReverseOrder: false,
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
 			
 			//目录加载
 			tocs: tocs(document)
