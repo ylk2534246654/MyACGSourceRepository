6e7d1597faf13b3bb6620687e581690c80d0ec80
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-12-24 19:02:58 +0800

    不再兼容 20231215 以下版本

--- sources/滴滴漫画.js
+++ sources/滴滴漫画.js
@@ -1,27 +1,23 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1660913240,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20230315,
+		minMyACG: 20231215,
 		
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 30,
 		
-		//是否启用失效#默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isEnabledInvalid: true,
+		enableInvalid: true,
 		
 		//@NonNull 搜索源名称
 		name: "滴滴漫画",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
@@ -32,24 +28,23 @@ function manifest() {
 
 		//搜索源自动同步更新网址
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/滴滴漫画.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/滴滴漫画.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/滴滴漫画.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/滴滴漫画.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/滴滴漫画.js",
 		},
 		
-		//更新时间
-		updateTime: "2023年3月15日",
+		//最近更新时间
+		lastUpdateTime: 1703407068,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 1,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 1,
 		
-		//自定义标签
-		groupName: ["漫画"],
+		//分组
+		group: ["漫画"],
 		
 		//@NonNull 详情页的基本网址
 		baseUrl: baseUrl,
@@ -62,20 +57,19 @@ const header = '@header->User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) A
 /**
  * 搜索
  * @param {string} key
- * @return {[{title, summary, cover, url}]}
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = ToolUtils.urlJoin(baseUrl,'/api/front/index/search@post->key=' + encodeURI(key) + header);
+	var url = JavaUtils.urlJoin(baseUrl,'/api/front/index/search@post->key=' + encodeURI(key) + header);
 	const response = HttpRequest(url);
 	var result = [];
 	if(response.code() == 200){
-		Log(response.html());
 		const $ = JSON.parse(response.html());
 		if($.code != 1){//此处和七夕漫画相似
 			$.data.forEach((child) => {
 				result.push({
 					//标题
-					title: child.name,
+					name: child.name,
 			
 					//概览
 					summary: child.content,
@@ -91,10 +85,10 @@ function search(key) {
 	}
 	return JSON.stringify(result);
 }
+
 /**
  * 详情
- * @param {string} url
- * @return {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(url) {
 	const response = HttpRequest(url + header);
@@ -102,7 +96,7 @@ function detail(url) {
 		var document = response.document();
 		return JSON.stringify({
 			//标题
-			title: document.selectFirst('.content > .title').text(),
+			name: document.selectFirst('.content > .title').text(),
 			
 			//作者
 			//author: document.selectFirst('').text(),
@@ -116,8 +110,8 @@ function detail(url) {
 			//封面网址
 			//coverUrl: document.selectFirst('').absUrl('src'),
 			
-			//是否启用将章节置为倒序
-			isEnabledChapterReverseOrder: false,
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
 			
 			//目录加载
 			tocs: tocs(document)
