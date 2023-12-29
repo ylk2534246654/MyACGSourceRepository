7854268e9a116245bca9dc4a9e617168b3540cf5
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-12-29 17:23:26 +0800

    Add files via upload

--- sources/OmoFun动漫.js
+++ sources/OmoFun动漫.js
@@ -1,121 +1,124 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://pan.baidu.com/s/1kVkWknH',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1654703176,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20230315,
+		minMyACG: 20230911,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 70,
 		
-		//是否启用失效#默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		isEnabledInvalid: true,
+		enableInvalid: true,
 		
 		//@NonNull 搜索源名称
 		name: "OmoFun动漫",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 3,
+		version: 4,
 
 		//搜索源自动同步更新网址
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/OmoFun动漫.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/OmoFun动漫.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/OmoFun动漫.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/OmoFun动漫.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/OmoFun动漫.js",
 		},
 		
-		//更新时间
-		updateTime: "2023年3月15日",
+		//最近更新时间
+		lastUpdateTime: 1694954345,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 1,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 1,
 		
-		//自定义标签
-		groupName: ["动漫"],
+		//分组
+		group: ["动漫"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "https://www.omofun.tv",
+		baseUrl: baseUrl,
 	});
 }
-const header = '';
+
+const baseUrl 	= "https://www.omofun.tv";
+const apiBaseUrl 	= "http://103.91.210.141:2515";
 
 /**
  * 搜索
  * @param {string} key
- * @return {[{title, summary, coverUrl, url}]}
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = `http://103.91.210.141:2515/xgapp.php/v2/search?pg=1&text=${encodeURI(key)}&token=&csrf=gqcMwzohO0l%2FcuKwQUeYY6TeY8l3M6D0SQwfwzNJTpvfNP2X30FiGs8um0tslhdkVxPTwLLXCGBkTA1ONXAXr6Ajx2fWb7fwHQdNnipq143De%2Brq%2FBAOrJ0Uqb9anV14kUbsbRr8P6ZKLG%2BANj%2BYZEyH%2Flu6a%2FAD4iekfTf9SA8%3D` + header;
-	const response = HttpRequest(url + header);
+	var url = JavaUtils.urlJoin(apiBaseUrl, `/xgapp.php/v2/search?pg=1&text=${encodeURI(key)}&token=&csrf=gqcMwzohO0l%2FcuKwQUeYY6TeY8l3M6D0SQwfwzNJTpvfNP2X30FiGs8um0tslhdkVxPTwLLXCGBkTA1ONXAXr6Ajx2fWb7fwHQdNnipq143De%2Brq%2FBAOrJ0Uqb9anV14kUbsbRr8P6ZKLG%2BANj%2BYZEyH%2Flu6a%2FAD4iekfTf9SA8%3D`);
 	var result = [];
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		const $ = JSON.parse(response.html());
-		$.data.forEach((child) => {
+		JSON.parse(response.body().string()).data.forEach((child) => {
 			result.push({
-				//标题
-				title: child.vod_name,
+				//名称
+				name: child.vod_name,
+
+				//最后章节名称
+				lastChapterName: child.vod_remarks,
+
+				//最近更新时间
+				lastUpdateTime: child.vod_time_add,
 				
 				//概览
-				summary: child.vod_class,
+				//summary: child.vod_class,
 				
-				//封面网址
-				coverUrl: child.vod_pic,
+				//封面
+				cover: child.vod_pic,
 				
 				//网址
-				url: child.vod_id,
-			})
-		})
+				url: child.vod_id
+			});
+		});
 	}
-	return JSON.stringify(result)
+	return JSON.stringify(result);
 }
 
 /**
  * 详情
- * @return {[{title, author, update, summary, coverUrl, isEnabledChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
 function detail(id) {
-	const response = HttpRequest(`http://103.91.210.141:2515/xgapp.php/v2/video_detail?id=${id}&ck=PmJag8SWhbuByo%2FjYOpOO44qrkaqIKk1Gv4qgT2q47g3X5yfdyUcBmcpQLhBiPmS%2B1uCpYypSeedNYTJjbyESEbIfTaSadOG9keaxpB%2BVmJSOs9Mnt65Xo6QIX4%2Fjtpu2ilHN8%2B1VcVdOHZtVZWYd5bNZg1FrhcsqR7geK9%2BfYkPEjFutBbu2gBmgYJtFsYYN%2FOlMNsEZq8nuZUsx%2Bts9fT%2FzFuQHHBypst5UmcfaJ5PpV0caKgWB2Gcok5NSNe%2B5%2BfZcDe05BwqUG1V6OHTz1iq9IL6zqbY1Y7ukAhdO%2FYYEzXnygWqheM52HU1zABjNDux3K1HpxI9IXvNI3ga4DIGf2EzEQUTcLehdSJQ1IwKK8ZOyqfON9r%2F2S4ZIwI9xGLVs7a9BRKv99Qlf6KxSmqMy%2F4qUEOjoqesuSHcQVga%2F4xq1hNm3B18rf8aVM1g6H7Nu0X1uXSKvCtGeQf5Tdg3omZfwK9BYlx4p0JMkzxapMYZiTOcqLkNdTW3LkSbfQEbgd7QDh9DmFBWceA40XwZgJuIIs1ZvD9DyYurHUVzfrgtP45dFkZ0W4vuWqeDgV30C6kvacoE4SQOYpvITE7UUdgTWRXW6xcZDJRB50QicehcAfYACRDJt32arqSGSiNoNT3MU7il3B%2B%2BbZUNMHe6WQfGDVJvJn1x2JIGVVdabCM1DCpK8nJuwqu6N7ycMN2%2B7QuJ%2FGrU5kze6DmQMid%2FH0JudcYhel3AgNAte81amrO559TvaK%2B5xaxns7XqHcAL4e3cLLX2lbh7uqwZoyNUgCXs2bX9BgPoOnNHJmDrxf4pbw57c6kKmhgEcKu4fYWldh9flAvJB3zztfcQUA%3D%3D&pkid=tv.omofun.app&token=&csrf=o13Tgl%2B3XOGfJjcTsCYGUhvnobNCR0v7njbsw2IooR%2FoArjKrCyogNFpYO9wNtn%2FNgFXBQL2DGFd3zDR7ekTMY%2B0QHU7QR%2Fyl6c%2Boa7A6l7op%2Fn6gtg0jzO7%2FSDDSAvuv56Sg8Y2wo%2FPxDf012ABHeVt8V1%2F3sRtzPvVTUg5t2M%3D`+ header);
+	var url = JavaUtils.urlJoin(apiBaseUrl, `/xgapp.php/v2/video_detail?id=${id}&ck=PmJag8SWhbuByo%2FjYOpOO44qrkaqIKk1Gv4qgT2q47g3X5yfdyUcBmcpQLhBiPmS%2B1uCpYypSeedNYTJjbyESEbIfTaSadOG9keaxpB%2BVmJSOs9Mnt65Xo6QIX4%2Fjtpu2ilHN8%2B1VcVdOHZtVZWYd5bNZg1FrhcsqR7geK9%2BfYkPEjFutBbu2gBmgYJtFsYYN%2FOlMNsEZq8nuZUsx%2Bts9fT%2FzFuQHHBypst5UmcfaJ5PpV0caKgWB2Gcok5NSNe%2B5%2BfZcDe05BwqUG1V6OHTz1iq9IL6zqbY1Y7ukAhdO%2FYYEzXnygWqheM52HU1zABjNDux3K1HpxI9IXvNI3ga4DIGf2EzEQUTcLehdSJQ1IwKK8ZOyqfON9r%2F2S4ZIwI9xGLVs7a9BRKv99Qlf6KxSmqMy%2F4qUEOjoqesuSHcQVga%2F4xq1hNm3B18rf8aVM1g6H7Nu0X1uXSKvCtGeQf5Tdg3omZfwK9BYlx4p0JMkzxapMYZiTOcqLkNdTW3LkSbfQEbgd7QDh9DmFBWceA40XwZgJuIIs1ZvD9DyYurHUVzfrgtP45dFkZ0W4vuWqeDgV30C6kvacoE4SQOYpvITE7UUdgTWRXW6xcZDJRB50QicehcAfYACRDJt32arqSGSiNoNT3MU7il3B%2B%2BbZUNMHe6WQfGDVJvJn1x2JIGVVdabCM1DCpK8nJuwqu6N7ycMN2%2B7QuJ%2FGrU5kze6DmQMid%2FH0JudcYhel3AgNAte81amrO559TvaK%2B5xaxns7XqHcAL4e3cLLX2lbh7uqwZoyNUgCXs2bX9BgPoOnNHJmDrxf4pbw57c6kKmhgEcKu4fYWldh9flAvJB3zztfcQUA%3D%3D&pkid=tv.omofun.app&token=&csrf=o13Tgl%2B3XOGfJjcTsCYGUhvnobNCR0v7njbsw2IooR%2FoArjKrCyogNFpYO9wNtn%2FNgFXBQL2DGFd3zDR7ekTMY%2B0QHU7QR%2Fyl6c%2Boa7A6l7op%2Fn6gtg0jzO7%2FSDDSAvuv56Sg8Y2wo%2FPxDf012ABHeVt8V1%2F3sRtzPvVTUg5t2M%3D`);
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		const $ = JSON.parse(response.html())
+		var $ = JSON.parse(response.body().string()).data.vod_info;
 		return JSON.stringify({
-			//标题
-			title : $.data.vod_info.vod_name,
+			//名称
+			name: $.vod_name,
 			
 			//作者
-			//author: $.data.author,
+			//author: ,
 			
-			//日期
-			update : $.data.vod_info.vod_time_add,
+			//最近更新时间
+			lastUpdateTime: $.vod_time_add,
 			
 			//概览
-			summary: $.data.vod_info.vod_content,
-	
-			//封面网址
-			cover : $.data.vod_info.vod_pic,
+			summary: $.vod_content,
 			
-			//是否启用将章节置为倒序
-			isEnabledChapterReverseOrder: false,
+			//封面
+			coverUrl: $.vod_pic,
+			
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
 			
 			//目录网址/非外链无需使用
-			tocs: tocs($.data.vod_info.vod_url_with_player)
+			tocs: tocs($.vod_url_with_player)
 		});
 	}
 	return null;
@@ -137,11 +140,15 @@ function tocs(tocs) {
 		var chapters = toc.url.split('#');
 		for(var i = 0;i < chapters.length;i++){
 			var chapter = chapters[i].split('$');
+			var url = chapter[1];
+			if(!JavaUtils.isValidUrl(url)){
+				url = "http://103.91.210.141:6100/jx.php?url=" + url;
+			}
 			newChapters.push({
 				//章节名称
 				name: chapter[0],
 				//章节网址
-				url: 'http://103.91.210.141:6100/jx.php?url=' + chapter[1]
+				url: url
 			})
 		}
 		
@@ -169,17 +176,15 @@ function tocs(tocs) {
 		]
 	}
 }
-
 /**
  * 内容(InterceptRequest)
- * @return {string} content
- */
+ * @params {string} url
+ * @returns {string} content
 function content(url) {
-	const response = HttpRequest(url + header);
+	const response = JavaUtils.httpRequest(url);
 	if(response.code() == 200){
-		const $ = JSON.parse(response.html())
-		//浏览器请求结果处理
-		return $.url;
+		return JSON.parse(response.body().string()).url;
 	}
 	return null;
 }
+ */
--- sources/ZzzFun动漫.js
+++ sources/ZzzFun动漫.js
@@ -1,160 +1,191 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1648714353,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220101,
+		minMyACG: 20230911,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
-		priority: 80,
+		//优先级 1~100，数值越大越靠前
+		priority: 70,
 		
-		//是否失效，默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		invalid: true,
+		enableInvalid: false,
 		
 		//@NonNull 搜索源名称
 		name: "ZzzFun动漫",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 4,
+		version: 5,
 
 		//搜索源自动同步更新网址
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/ZzzFun动漫.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/ZzzFun动漫.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/ZzzFun动漫.js",
-			"Coding": "https://ylk2534246654.coding.net/p/myacg/d/MyACGSourceRepository/git/raw/master/sources/ZzzFun动漫.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/ZzzFun动漫.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/ZzzFun动漫.js",
 		},
 		
-		//更新时间
-		updateTime: "2022年3月29日",
+		//最近更新时间
+		lastUpdateTime: 1696062359,
 		
-		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
+		//@NonNull 类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
 		
-		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 2,
+		//@NonNull 内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 2,
 		
-		//自定义标签
-		tag: ["动漫"],
+		//分组
+		group: ["动漫"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "http://service-i86gk1am-1251249846.gz.apigw.tencentcs.com",
+		baseUrl: "http://www.zzzfun.one",
+		
+		//启用用户登录
+		enableUserLogin: true,
+		
+		//用户登录网址
+		userLoginUrl: "http://www.zzzfun.one/index.php/user_login.html",
+		
+		//需要用户登录列表（search，detail，content，find）
+		requiresUserLoginList: ["content"],
 	});
 }
-const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
+const baseUrl 	= "http://service-i86gk1am-1251249846.gz.apigw.tencentcs.com";
+
+const header = '@header->Timestamp: 1696059843192@header->Authorization: 43dcb64dad05238440dd02b5feb7c327@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
+
+/*
+ * 是否完成登录
+ * @param {string} url		网址
+ * @param {string} responseHtml	响应源码
+ * @return {boolean}  登录结果
+ */
+function isUserLoggedIn(url, responseHtml) {
+	if(url != null && responseHtml.indexOf('我的收藏') != -1){
+		return true;
+	}
+	return false;
+}
+/*
+ * 验证完成登录
+ * @return {boolean} 登录结果
+ */
+function verifyUserLoggedIn() {
+	const response = JavaUtils.httpRequest('http://www.zzzfun.one/index.php/user_login.html');
+	if(response.code() == 200){
+		if(response.body().string().indexOf('我的收藏') != -1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	return true;
+}
 
 /**
  * 搜索
- * @params {string} key
- * @returns {[{title, summary, cover, url}]}
+ * @param {string} key
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = 'http://service-i86gk1am-1251249846.gz.apigw.tencentcs.com/android/search@post->key='+ encodeURI(key) + header;
-	const response = httpRequest(url);
-	
-	const list = jsonPathArray(response,'$..data[*]');
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
-			//标题
-			title : jsonPath(data,'$.videoName'),
+	var url = JavaUtils.urlJoin(baseUrl, `/and116/search@post->key=${encodeURI(key)}`) + header;
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const $ = JSON.parse(response.body().string());
+		$.data.forEach((child) => {
+			result.push({
+				//名称
+				name: child.videoName,
 			
-			//概览
-			summary : jsonPath(data,'$.videoremarks'),
-			
-			//封面
-			cover : jsonPath(data,'$.videoImg'),
-			
-			//网址
-			url : 'http://service-i86gk1am-1251249846.gz.apigw.tencentcs.com/android/video/list_ios?videoId='+jsonPath(data,'$.videoId')
-			});
+				//最后章节名称
+				lastChapterName: child.videoremarks,
+				
+				//概览
+				summary: child.videoClass,
+				
+				//封面 
+				coverUrl: child.videoImg,
+				
+				//网址
+				url: child.videoId
+			})
+		});
 	}
-	return JSON.stringify(array);
+	return JSON.stringify(result);
 }
+
 /**
  * 详情
- * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
  */
-function detail(url) {
-	const response = httpRequest(url+ header);
-	return JSON.stringify({
-		//标题
-		title: jsonPath(response,'$.data.videoName'),
-		
-		//作者
-		author: jsonPath(response,'$.data.up'),
-		
-		//概览
-		summary: jsonPath(response,'$.data.videoDoc'),
-
-		//封面
-		cover : jsonPath(response,'$.data.videoImg'),
-		
-		//目录是否倒序
-		reverseOrder: false,
-		
-		//目录网址/非外链无需使用
-		catalog: catalog(response,url)
-	})
+function detail(id) {
+	var url = JavaUtils.urlJoin(baseUrl, `/and116/video/list_video?userid=622214&videoId=${id}`) + header;
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		var $ = JSON.parse(response.body().string());
+		return JSON.stringify({
+			//名称
+			name: $.data.videoName,
+			
+			//作者
+			author: $.data.up,
+			
+			//概览
+			summary: $.data.videoDoc,
+			
+			//封面
+			coverUrl: $.data.videoImg,
+			
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
+			
+			//目录
+			tocs: tocs($.data.videoSets)
+		});
+	}
+	return null;
 }
+
 /**
  * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
+ * @returns {[{name, chapters:{[{name, url}]}}]}
  */
-function catalog(response,url) {
-	//分组代码
-	const videoSets = jsonPathArray(response,'$.data.videoSets[*]');
-	
+function tocs(videoSets) {
 	//创建目录数组
-	var new_catalogs= [];
-	
-	for (var i = 0;i<videoSets.length;i++) {
-	    var catalog = videoSets[i];
-		
+	var newTocs = [];
+
+	var catalogi = 0;
+	videoSets.forEach((catalog) => {
+		catalogi ++;
 		//创建章节数组
-		var newchapters= [];
+		var newChapters = [];
 		
-		//章节代码
-		var chapters = jsonPathArray(catalog,'$.list[*]');
-		
-		for (var ci=0;ci<chapters.length;ci++) {
-			var chapter = chapters[ci];
-			
-			var playids = jsonPath(chapter,'$.playid').split('-');
-			
-			newchapters.push({
+		catalog.list.forEach((chapter) => {
+			var playids = chapter.playid.split('-');
+			newChapters.push({
 				//章节名称
-				name: jsonPath(chapter,'$.ji'),
+				name: chapter.ji,
 				//章节网址
-				url: 'http://www.zzzfun.com/vod_play_id_'+playids[0]+'_sid_'+(i+1)+'_nid_'+playids[1]+'.html'
+				url: `http://www.zzzfun.one/vod_play_id_${playids[0]}_sid_${catalogi}_nid_${playids[1]}.html`
+				//url: JavaUtils.urlJoin(baseUrl, `/and116/video/116play@post->playid=${chapter.playid}&sing=ef8b9efb7f5fcbae71578e44e2cead72&userid=622214&apptoken=2e6ae28032480d50aff549671218c82d&map=1696061565710`)
 			});
-		}
-		//添加目录
-		new_catalogs.push({
+		});
+		newTocs.push({
 			//目录名称
-			tag: jsonPath(videoSets[i],'$.load'),
+			name: catalog.load,
 			//章节
-			chapter : newchapters
-			});
-	}
-	return new_catalogs
+			chapters : newChapters
+		});
+	});
+	return newTocs
 }
