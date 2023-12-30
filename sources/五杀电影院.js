96c13c228ed14eb5d53c92e1fa015643054da308
雨夏 <36308416+ylk2534246654@users.noreply.github.com>
2023-12-30 15:10:50 +0800

    Add files via upload

--- sources/五杀电影院.js
+++ sources/五杀电影院.js
@@ -1,165 +1,173 @@
 function manifest() {
 	return JSON.stringify({
-		//MyACG 最新版本
-		MyACG: 'https://lanzou.com/b07xqlbxc ',
-		
 		//@NonNull 搜索源 ID 标识，设置后不建议更改
 		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
 		id: 1651916756,
 		
 		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
-		minMyACG: 20220101,
+		minMyACG: 20230911,
 
-		//优先级1~100，数值越大越靠前
-		//参考：搜索结果多+10，响应/加载速度快+10，品质优秀+10，更新速度快+10，有封面+10，无需手动授权+10
+		//优先级 1~100，数值越大越靠前
 		priority: 1,
 		
-		//是否失效，默认关闭
+		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		invalid: true,
+		enableInvalid: true,
 		
 		//@NonNull 搜索源名称
 		name: "五杀电影院",
 
-		//搜索源制作人
+		//搜索源作者
 		author: "雨夏",
 
 		//电子邮箱
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 2,
+		version: 3,
 
 		//搜索源自动同步更新链接
 		syncList: {
-			"Gitee":  "https://gitee.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/五杀电影院.js",
 			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/五杀电影院.js",
 			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/五杀电影院.js",
 			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/五杀电影院.js",
 			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/五杀电影院.js",
 		},
 		
-		//更新时间
-		updateTime: "2022年3月29日",
+		//最近更新时间
+		lastUpdateTime: 1695218158,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
 		
-		//内容处理方式： -1: 搜索相似，0：对链接处理并调用外部APP访问，1：对链接处理，2：对内部浏览器拦截的请求处理，3：对内部浏览器拦截的框架处理
-		contentType: 2,
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 2,
 		
-		//自定义标签
-		tag: ["影视"],
+		//分组
+		group: ["影视"],
 		
 		//@NonNull 详情页的基本网址
-		baseUrl: "https://www.lol5s.com",
+		baseUrl: baseUrl,
+
+		//全局 HTTP 请求头列表
+		httpRequestHeaderList: {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+		}
 	});
 }
-const header = '@header->user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36';
+
+const baseUrl = "https://www.lol5s.com";
 
 /**
  * 搜索
- * @params {string} key
- * @returns {[{title, summary, cover, url}]}
+ * @param {string} key
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
  */
 function search(key) {
-	var url = 'https://www.lol5s.com/index.php?m=vod-search@post->wd='+ encodeURI(key) + header;
-	const response = httpRequest(url);
-	
-	const list = jsoupArray(response,'div.container > div.color > ul > li').outerHtml();
-	var array= [];
-	for (var i=0;i<list.length;i++) {
-	    var data = list[i];
-		array.push({
-			//标题
-			title : jsoup(data,'label.name').text(),
-			
-			//概览
-			//summary : jsoup(data,'p:nth-child(3) > a').text(),
-			
-			//封面
-			cover : ToolUtil.urlJoin(url,jsoup(data,'li > a > img').attr('data-original')),
-			
-			//网址
-			url : ToolUtil.urlJoin(url,jsoup(data,'li > a').attr('href'))
-			});
-	}
-	return JSON.stringify(array);
-}
-/**
- * 详情
- * @params {string} url
- * @returns {[{title, author, date, summary, cover, reverseOrder, catalog:{[{tag, chapter:{[{name, url}]}}]}}]}
- */
-function detail(url) {
-	const response = httpRequest(url+ header);
-	return JSON.stringify({
-		//标题
-		title : jsoup(response,'div.color > div.butte > h1').text(),
-		
-		//作者
-		//author: jsoup(response,'#page-back > div > div > div.sidebar-left > div.normal-wai1 > div > div.normal-nei1.dhxx.anime_info > p:nth-child(7)').text(),
-		
-		//更新时间
-		date: jsoup(response,'div.row > dl:nth-child(1) > dd:nth-child(3)').text(),
-		
-		//概览
-		summary: jsoup(response,'dd > p').text(),
+	var url = JavaUtils.urlJoin(baseUrl, '/index.php?m=vod-search@post->wd=' + encodeURI(key));
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		var elements = document.select("div.container > div.color > ul > li");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('label.name').text(),
+				
+				//最后章节名称
+				//lastChapterName: element.selectFirst('.video_play_status').text(),
 
-		//封面
-		cover : ToolUtil.urlJoin(url,jsoup(response,'div.color > div:nth-child(4) > div > img').attr('data-original')),
+				//最近更新时间
+				//lastUpdateTime: element.selectFirst('.video_play_status').text(),
 
-		//目录是否倒序
-		reverseOrder: true,
-		
-		//目录链接/非外链无需使用
-		catalog: catalog(response,url)
-	})
-}
-/**
- * 目录
- * @params {string} response
- * @params {string} url
- * @returns {[{tag, chapter:{[{name, url}]}}]}
- */
-function catalog(response,url) {
-	//目录标签代码
-	const tabs = jsoupArray(response,'div.cc > #myTab').outerHtml();
-	
-	//目录代码
-	const catalogs = jsoupArray(response,'div.cc > #myTabContent').outerHtml();
-	
-	//创建目录数组
-	var new_catalogs= [];
-	
-	for (var i=0;i<catalogs.length;i++) {
-	    var catalog = catalogs[i];
-		
-		//创建章节数组
-		var newchapters= [];
-		
-		//章节代码
-		var chapters = jsoupArray(catalog,'li').outerHtml();
-		
-		for (var ci=0;ci<chapters.length;ci++) {
-			var chapter = chapters[ci];
-			
-			newchapters.push({
-				//章节名称
-				name: jsoup(chapter,'a').text(),
-				//章节链接
-				url: ToolUtil.urlJoin(url,jsoup(chapter,'a').attr('href'))
+				//概览
+				//summary: element.selectFirst('.desc > :matchText').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('li > a > img').absUrl('data-original'),
+				
+				//网址
+				url: element.selectFirst('li > a').absUrl('href')
 			});
 		}
-		//添加目录
-		new_catalogs.push({
-			//目录名称
-			tag: jsoup(tabs[i],'li').text(),
-			//章节
-			chapter : newchapters
-			});
 	}
-	return new_catalogs
+	return JSON.stringify(result);
+}
+
+/**
+ * 详情
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ */
+function detail(url) {
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		return JSON.stringify({
+			//标题
+			name: document.selectFirst('div.color > div.butte > h1').text(),
+			
+			//作者
+			//author: document.selectFirst('').text(),
+			
+			//最近更新时间
+			lastUpdateTime: document.selectFirst('div.row > dl:nth-child(1) > dd:nth-child(3)').text(),
+			
+			//概览
+			summary: document.selectFirst('dd > p').text(),
+	
+			//封面网址
+			coverUrl: document.selectFirst('div.color > div:nth-child(4) > div > img').absUrl('data-original'),
+			
+			//启用章节反向顺序
+			enableChapterReverseOrder: true,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
+}
+
+/**
+ * 目录
+ * @return {[{name, chapters:{[{name, url}]}}]}
+ */
+function tocs(document) {
+	const tagElements = document.select('div.cc > #myTab');
+	
+	//目录元素选择器
+	const tocElements = document.select('div.cc > #myTabContent');
+	
+	//创建目录数组
+	var newTocs = [];
+	
+	for (var i = 0;i < tocElements.size();i++) {
+		//创建章节数组
+		var newChapters = [];
+		
+		//章节元素选择器
+		var chapterElements = tocElements.get(i).select('li');
+		
+		for (var i2 = 0;i2 < chapterElements.size();i2++) {
+			var chapterElement = chapterElements.get(i2);
+			
+			newChapters.push({
+				//章节名称
+				name: chapterElement.selectFirst('a').text(),
+				//章节网址
+				url: chapterElement.selectFirst('a').absUrl('href')
+			});
+		}
+		newTocs.push({
+			//目录名称
+			name: tagElements.get(i).selectFirst('li').text(),
+			//章节
+			chapters: newChapters
+		});
+	}
+	return newTocs
 }
 /**
  * 内容(InterceptRequest)
