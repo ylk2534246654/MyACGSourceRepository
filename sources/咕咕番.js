c250e43531682486030a8cc1932f0cacb8112977
ylk2534246654 <2534246654@qq.com>
2024-01-04 22:17:51 +0800

    新增

---
+++ sources/咕咕番.js
@@ -0,0 +1,295 @@
+function manifest() {
+	return JSON.stringify({
+		//@NonNull 搜索源 ID 标识，设置后不建议更改
+		//可前往https://tool.lu/timestamp/ 生成时间戳（精确到秒）
+		id: 1704369869,
+		
+		//最低兼容MyACG版本（高版本无法安装在低版本MyACG中）
+		minMyACG: 20240104,
+
+		//优先级 1~100，数值越大越靠前
+		priority: 80,
+		
+		//启用失效#默认关闭
+		//true: 无法安装，并且已安装的变灰，用于解决失效源
+		enableInvalid: false,
+		
+		//@NonNull 搜索源名称
+		name: "咕咕番",
+
+		//搜索源作者
+		author: "雨夏",
+
+		//电子邮箱
+		email: "2534246654@qq.com",
+
+		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
+		version: 1,
+
+		//搜索源自动同步更新网址
+		syncList: {
+			"极狐":   "https://jihulab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/咕咕番.js",
+			"Gitlab": "https://gitlab.com/ylk2534246654/MyACGSourceRepository/-/raw/master/sources/咕咕番.js",
+			"Github": "https://github.com/ylk2534246654/MyACGSourceRepository/raw/master/sources/咕咕番.js",
+			"Gitcode":"https://gitcode.net/Cynric_Yx/MyACGSourceRepository/-/raw/master/sources/咕咕番.js",
+		},
+		
+		//最近更新时间
+		lastUpdateTime: 1704369869,
+		
+		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
+		type: 3,
+		
+		//内容处理方式： -1: 搜索相似，0：对网址处理并调用外部APP访问，1：对网址处理，2：对内部浏览器拦截
+		contentProcessType: 2,
+		
+		//分组
+		group: ["动漫"],
+		
+		//@NonNull 详情页的基本网址
+		baseUrl: baseUrl,
+
+		//发现
+		findList: {
+			category: {
+				"genre": {
+                    "连载新番": "6",
+                    "完结动画": "7",
+                    "动漫电影": "21",
+                },
+				"label": ["全部","运动","热血","剧情","奇幻","游戏","校园","战斗","恋爱","励志","后宫","悬疑","泡面番","神魔","百合","青春","职场","战争"],
+				"year": ["全部","2024","2023","2022","2021","2020","2019","2018","2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000"],
+                "order": {
+					"时间排序": "time",
+					"人气排序": "hits",
+					"评分排序": "score",
+				},
+			},
+			"动漫": ["genre", "year", "label", "order"]
+		},
+
+		//全局 HTTP 请求头列表
+		httpRequestHeaderList: {
+			"Referer": baseUrl
+		}
+	});
+}
+
+const baseUrl = "https://www.gugufan.com"
+/**
+ * Qqun:832557128 
+ */
+
+/**
+ * 搜索
+ * @param {string} key
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
+ */
+function search(key) {
+	var url = JavaUtils.urlJoin(baseUrl, '/index.php/vod/search.html?wd=' + encodeURI(key));
+	var result = [];
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		var elements = document.select(".search-box");
+		for (var i = 0;i < elements.size();i++) {
+			var element = elements.get(i);
+			result.push({
+				//名称
+				name: element.selectFirst('.thumb-txt').text(),
+				
+				//最后章节名称
+				lastChapterName: element.selectFirst('.public-list-prb').text(),
+
+				//概览
+				summary: element.selectFirst('.thumb-blurb').text(),
+				
+				//封面网址
+				coverUrl: element.selectFirst('.lazy').absUrl('data-src'),
+				
+				//网址
+				url: element.selectFirst('a').absUrl('href')
+			});
+		}
+	}
+	return JSON.stringify(result);
+}
+
+/**
+ * 发现
+ * @return {[{name, author, lastChapterName, lastUpdateTime, summary, coverUrl, url}]}
+ */
+function find(genre, year, label, order) {
+	if(genre == "全部")genre = "";
+	if(label == "全部")label = "";
+	if(year == "全部")year = "";
+	
+	var result = [];
+	const timestamp = Math.round(new Date().getTime() / 1000).toString();
+	//DCC147D11943AF75 = EC['Pop']['Uid']
+	var url = JavaUtils.urlJoin(baseUrl, `/index.php/api/vod@post->type=${genre}&class=${label}&area=&lang=&version=&state=&letter=&page=1&time=${timestamp}&by=${order}&year=${year}&key=${JavaUtils.bytesToHexString(JavaUtils.encryptMD5(`DS${timestamp}DCC147D11943AF75`), false)}`);
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const $ = JSON.parse(response.body().string());
+		$.list.forEach((child) => {
+			if(child.vod_remarks.indexOf("开播") == -1){//表示已开播作品
+				result.push({
+					//名称
+					name: child.vod_name,
+			
+					//作者
+					author: child.vod_actor,
+					
+					//最近更新时间
+					//lastUpdateTime: ,
+	
+					//概览
+					summary: child.vod_blurb,
+			
+					//封面网址
+					coverUrl: child.vod_pic,
+			
+					//网址
+					url: JavaUtils.urlJoin(baseUrl, `/index.php/vod/detail/id/${child.vod_id}.html`)
+				});
+			}
+		});
+
+	}
+	return JSON.stringify(result);
+}
+
+/**
+ * 详情
+ * @return {[{name, author, lastUpdateTime, summary, coverUrl, enableChapterReverseOrder, tocs:{[{name, chapter:{[{name, url}]}}]}}]}
+ */
+function detail(url) {
+	const response = JavaUtils.httpRequest(url);
+	if(response.code() == 200){
+		const document = response.body().cssDocument();
+		return JSON.stringify({
+			//标题
+			name: document.selectFirst('.slide-info-title').text(),
+			
+			//概览
+			summary: document.selectFirst('#height_limit').text(),
+	
+			//封面网址
+			coverUrl: document.selectFirst('.detail-pic > img').absUrl('data-src'),
+			
+			//启用章节反向顺序
+			enableChapterReverseOrder: false,
+			
+			//目录加载
+			tocs: tocs(document)
+		});
+	}
+	return null;
+}
+
+/**
+ * 目录
+ * @returns {[{name, chapters:{[{name, url}]}}]}
+ */
+function tocs(document) {
+	//目录标签元素选择器
+	const tagElements = document.select('.anthology-tab > div > a');
+	
+	//目录元素选择器
+	const tocElements = document.select('.anthology-list-play');
+	
+	//创建目录数组
+	var newTocs = [];
+	
+	for (var i = 0;i < tocElements.size();i++) {
+		//创建章节数组
+		var newChapters = [];
+		
+		//章节元素选择器
+		var chapterElements = tocElements.get(i).select('ul > li');
+		
+		for (var i2 = 0;i2 < chapterElements.size();i2++) {
+			var chapterElement = chapterElements.get(i2);
+			newChapters.push({
+				//章节名称
+				name: chapterElement.selectFirst('a').text(),
+				//章节网址
+				url: chapterElement.selectFirst('a').absUrl('href')
+			});
+		}
+		newTocs.push({
+			//目录名称
+			name: tagElements.get(i).text(),
+			//章节
+			chapters : newChapters
+		});
+	}
+	return newTocs;
+}
+
+
+/**
+ * 内容（部分搜索源通用过滤规则）
+ * @version 2024/1/4
+ * 布米米、嘻嘻动漫、12wo动漫、路漫漫、风车动漫P、樱花动漫P、COCO漫画、Nike、cocoManga
+ * @return {string} content
+ */
+function content(url) {
+	var re = new RegExp(
+		//https://
+		'(^[a-zA-z]+://[^\\s/]+/(' +
+
+		//https://knr.xxxxx.cn/j/140000		#[a-z]{1}\/\d{6}
+		'([a-z]{1}/\\d)' +
+
+		//https://xx.xxx.xx/xxx/xxx/0000	#[a-z]{3}\/[a-z]{3}\/\d
+		'|([a-z]{3}/[a-z]{3}/\\d)' +
+
+		//https://tg.xxx.com/sc/0000?n=xxxx #[a-z]{2}\/\d{4}\?
+		'|([a-z]{2}/\\d{4}\\?)' +
+
+		//https://xx.xxx.xyz/vh1/158051 	#[\w]{3}\/\d{6}$
+		'|([\\w]{3}/\\d{6}$)' +
+
+		//https://xx.xx.com/0000/00/23030926631.txt 	#[\d]{4}\/\d{2}\/\d{11}\.txt
+		'|([\\d]{4}/\\d{2}/\\d{11}\\.txt)' +
+
+		//https://xxx.com/ba4fa4f070f761b057fbabfb3fd7925d.txt 	#\w{32}\.txt
+		'|(\\w{32}\\.txt)' +
+		
+		//https://zbg.xxx.com/candy14395.js 	#\w{32}\.txt
+		'|(candy\\d{5}\\.)' +
+
+		//https://xxxxx.xxxxxx.com/v2/stats/12215/157527 	#[\w]{2}\/\w{5}\/\d{5}\/\d{6}
+		'|([\\w]{2}/\\w{5}/\\d{5}/\\d{6})' +
+
+		//https://xxx.xxxxxx.com/sh/to/853	#sh\/[\w]{2}\/\d{3}
+		'|(sh/[\\w]{2}/\\d{3})' +
+
+		//https://xxx.rmb.xxxxxxxx.com/xxx/e3c5da206d50f116fc3a8f47502de66d.gif #[\w]{3}\/[\w]{32}\.
+		'|([\\w]{3}/[\\w]{32}\\.)' +
+
+		//https://xxxx.xxxx.xx:00000/mnrt/kmrr1.woff
+		//https://xxxx.xxxx.xx:00000/kmopef/3.woff # [\w/]+[/km][\w/]+\.woff
+		'|([\\w/]+[/km][\\w/]+\\.woff)' +
+
+		//https://aba.xxxxxxx.cn/slot?2377029035902478992-27158		#slot\?[\d-]+$
+		'|(slot\\?[\\d-]+$)' +
+
+		//https://xxxx.xxxx.com/o.js # o\.js
+		//'|o\\.js' + //无法正常加载
+
+		//（!易误拦截） 例子过长，无法展示		#[\\w]{3}\?[\\S]{500,}
+		'|([\\w]{3}\?[\\S]{500,})' +
+		
+		'))' +
+        //Google
+		'|(^[a-zA-z]+://[^\\s/]+doubleclick\\.net/)'
+		,
+		'i'
+	);
+	if(!re.test(url)){
+		return url;
+	}
+	return null;
+}
