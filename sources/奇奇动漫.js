ba6cf7d09d407e5bec0a7a7744c921ba4f2b4488
ylk2534246654 <2534246654@qq.com>
2024-03-10 15:02:05 +0800

    修复

--- sources/奇奇动漫.js
+++ sources/奇奇动漫.js
@@ -43,7 +43,7 @@ function manifest() {
 		},
 		
 		//最近更新时间
-		lastUpdateTime: 1705284208,
+		lastUpdateTime: 1710054106,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
@@ -75,11 +75,12 @@ function manifest() {
 		},
 	});
 }
-const baseUrl = "http://qiqidm7.com";
+const baseUrl = "https://qiqidm7.com";
 /**
  * https://www.qiqidongman.com
  * https://www.qiqidongman.cc
  * qiqidm6.com
+ * qiqidm8.com
  */
 
 /**
