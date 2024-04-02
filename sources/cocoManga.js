a927b42393c2d627be8a02ecf1f7def13bb54e4e
ylk2534246654 <2534246654@qq.com>
2024-04-02 11:11:45 +0800

    修复

--- sources/cocoManga.js
+++ sources/cocoManga.js
@@ -24,7 +24,7 @@ function manifest() {
 		email: "2534246654@qq.com",
 
 		//搜索源版本号，低版本搜索源无法覆盖安装高版本搜索源
-		version: 17,
+		version: 18,
 
 		//自述文件网址
 		readmeUrlList: [
@@ -43,7 +43,7 @@ function manifest() {
 		},
 
 		//最近更新时间
-		lastUpdateTime: 1706922297,
+		lastUpdateTime: 1712027480,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 2,
