d5b2117a3be73ead0bc42639d2cc341438c2a836
ylk2534246654 <2534246654@qq.com>
2025-03-30 18:02:36 +0800

    设为失效

--- sources/怡萱动漫.js
+++ sources/怡萱动漫.js
@@ -12,7 +12,7 @@ function manifest() {
 		
 		//启用失效#默认关闭
 		//true: 无法安装，并且已安装的变灰，用于解决失效源
-		enableInvalid: false,
+		enableInvalid: true,
 		
 		//@NonNull 搜索源名称
 		name: "怡萱动漫",
@@ -41,7 +41,7 @@ function manifest() {
 		},
 		
 		//最近更新时间
-		lastUpdateTime: 1725783627,
+		lastUpdateTime: 1743328942,
 		
 		//默认为1，类别（1:网页，2:图库，3:视频，4:书籍，5:音频，6:图片）
 		type: 3,
